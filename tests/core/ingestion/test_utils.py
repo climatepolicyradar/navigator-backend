c6fb5edfdb59797427495cd46369fb89362e2df8
diversemix <diversemix@gmail.com>
2023-02-21 14:28:03 +0000

    fix, tidy and complete tests for get_or_create

--- tests/core/ingestion/test_utils.py
+++ tests/core/ingestion/test_utils.py
@@ -1,7 +1,17 @@
+from unittest.mock import MagicMock
 from app.core.ingestion.utils import get_or_create
 from app.data_migrations import populate_geography
 from app.db.models.law_policy.family import Family, FamilyCategory, FamilyStatus, Slug
 from sqlalchemy.orm import Session
+
+
+"""
+Note from author:
+
+The model of "Family" was chosen as its a little more complicated
+than the other models. It includes a "relationship" with Slug,
+which in turns requires that the Family has at least one Slug.
+"""
 
 
 def add_a_family(db: Session):
@@ -9,19 +19,27 @@ def add_a_family(db: Session):
         import_id="1",
         title="title",
         geography_id=2,
-        category_name=FamilyCategory.EXECUTIVE,
+        category_name="EXECUTIVE",
         description="description",
-        family_status=FamilyStatus.PUBLISHED,
+        family_status="Published",
     )
+    db.add(family)
+    add_a_slug_for_family1_and_flush(db)
+    return family
+
+
+def add_a_slug_for_family1_and_flush(db):
     slug = Slug(
         name="title_adb4",
         family_import_id="1",
         family_document_import_id=None,
     )
-    db.add(family)
     db.add(slug)
     db.flush()
-    return family
+    # NOTE: Creating the Slug is part of test init,
+    #      as we need a Slug to query for the Family.
+    slug = db.query(Slug).one()
+    assert slug
 
 
 def test_get_or_create__gets(test_db: Session):
@@ -39,14 +57,13 @@ def test_get_or_create__gets(test_db: Session):
     assert family.title == "title"
     assert family.description == "description"
     assert family.geography_id == 2
-    assert family.category_name == FamilyCategory.EXECUTIVE
-    assert family.family_status == FamilyStatus.PUBLISHED
-    # TODO: These fail
-    # assert family.category_name == "EXECUTIVE"
-    # assert family.family_status == "PUBLISHED"
+    assert family.category_name == "EXECUTIVE"
+    assert family.family_status == "Published"
+    assert FamilyCategory(family.category_name) == FamilyCategory.EXECUTIVE
+    assert FamilyStatus(family.family_status) == FamilyStatus.PUBLISHED
 
 
-def test_get_or_create__extra_args(test_db):
+def test_get_or_create__creates(test_db):
     populate_geography(test_db)
     new_family = get_or_create(
         test_db,
@@ -57,30 +74,42 @@ def test_get_or_create__extra_args(test_db):
             "geography_id": 2,
             "category_name": "EXECUTIVE",
             "description": "description",
-            "family_status": "PUBLISHED",
+            "family_status": "Published",
         },
     )
-    new_slug = get_or_create(
-        test_db,
-        Slug,
-        name="title_23a1",
-        extra={
-            "family_import_id": "1",
-            "family_document_import_id": None,
-        },
-    )
+    add_a_slug_for_family1_and_flush(test_db)
 
     family = test_db.query(Family).one()
-    slug = test_db.query(Slug).one()
-
-    assert slug == new_slug
     assert family
     assert family == new_family
     assert family.title == "title"
     assert family.description == "description"
     assert family.geography_id == 2
     assert family.category_name == "EXECUTIVE"
-    assert family.family_status == "PUBLISHED"
-    # TODO: These fail
-    # assert family.category_name == FamilyCategory.EXECUTIVE
-    # assert family.family_status == FamilyStatus.PUBLISHED
+    assert family.family_status == "Published"
+    assert FamilyCategory(family.category_name) == FamilyCategory.EXECUTIVE
+    assert FamilyStatus(family.family_status) == FamilyStatus.PUBLISHED
+
+
+def test_get_or_create__after_create(test_db):
+    populate_geography(test_db)
+    after_create = MagicMock()
+    new_family = get_or_create(
+        test_db,
+        Family,
+        import_id="1",
+        extra={
+            "title": "title",
+            "geography_id": 2,
+            "category_name": "EXECUTIVE",
+            "description": "description",
+            "family_status": "Published",
+        },
+        after_create=after_create,
+    )
+    add_a_slug_for_family1_and_flush(test_db)
+
+    family = test_db.query(Family).one()
+    assert family
+    assert family == new_family
+    after_create.assert_called_once_with(new_family)
