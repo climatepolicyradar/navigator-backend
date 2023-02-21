43ded2a21b430a856d88b7d6d80877fa6b5e4d4d
Joel Wright <joel@climatepolicyradar.org>
2023-02-21 13:04:04 +0000

    Families require slugs

--- tests/core/ingestion/test_utils.py
+++ tests/core/ingestion/test_utils.py
@@ -1,6 +1,6 @@
 from app.core.ingestion.utils import get_or_create
 from app.data_migrations import populate_geography
-from app.db.models.law_policy.family import Family
+from app.db.models.law_policy.family import Family, FamilyCategory, FamilyStatus, Slug
 from sqlalchemy.orm import Session
 
 
@@ -9,22 +9,25 @@ def add_a_family(db: Session):
         import_id="1",
         title="title",
         geography_id=2,
-        category_name="EXECUTIVE",
+        category_name=FamilyCategory.EXECUTIVE,
         description="description",
-        family_status="PUBLISHED",
+        family_status=FamilyStatus.PUBLISHED,
+    )
+    slug = Slug(
+        name="title_adb4",
+        family_import_id="1",
+        family_document_import_id=None,
     )
     db.add(family)
-    db.commit()
+    db.add(slug)
+    db.flush()
+    return family
 
 
 def test_get_or_create__gets(test_db: Session):
     populate_geography(test_db)
-    test_db.commit()
-    add_a_family(test_db)
-    test_db.commit()
-
-    # TODO: Remove the following check
-    assert test_db.query(Family).one()
+    test_db.flush()
+    existing_family = add_a_family(test_db)
 
     family = get_or_create(
         test_db,
@@ -32,16 +35,20 @@ def test_get_or_create__gets(test_db: Session):
         import_id="1",
     )
     assert family
+    assert family == existing_family
     assert family.title == "title"
     assert family.description == "description"
     assert family.geography_id == 2
-    assert family.category_name == "EXECUTIVE"
-    assert family.family_status == "PUBLISHED"
+    assert family.category_name == FamilyCategory.EXECUTIVE
+    assert family.family_status == FamilyStatus.PUBLISHED
+    # TODO: These fail
+    # assert family.category_name == "EXECUTIVE"
+    # assert family.family_status == "PUBLISHED"
 
 
 def test_get_or_create__extra_args(test_db):
     populate_geography(test_db)
-    family = get_or_create(
+    new_family = get_or_create(
         test_db,
         Family,
         import_id="1",
@@ -53,9 +60,27 @@ def test_get_or_create__extra_args(test_db):
             "family_status": "PUBLISHED",
         },
     )
+    new_slug = get_or_create(
+        test_db,
+        Slug,
+        name="title_23a1",
+        extra={
+            "family_import_id": "1",
+            "family_document_import_id": None,
+        },
+    )
+
+    family = test_db.query(Family).one()
+    slug = test_db.query(Slug).one()
+
+    assert slug == new_slug
     assert family
+    assert family == new_family
     assert family.title == "title"
     assert family.description == "description"
     assert family.geography_id == 2
     assert family.category_name == "EXECUTIVE"
     assert family.family_status == "PUBLISHED"
+    # TODO: These fail
+    # assert family.category_name == FamilyCategory.EXECUTIVE
+    # assert family.family_status == FamilyStatus.PUBLISHED
