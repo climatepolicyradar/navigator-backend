73b3ec122b3b14270b5bac736857e7e53b97de24
diversemix <diversemix@gmail.com>
2023-04-18 11:41:15 +0100

    Improve with cache

--- check_cdn/check_cdn.sh
+++ check_cdn/check_cdn.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-rm cdn_urls_input.txt 
-rm cdn_urls_missing.csv  # Contains missing CDN objects from CCLW
-rm cdn_urls_results.txt
+rm_existing() {
+  [ -f $1 ] && rm $1 || echo "$1 Clean"
+}
+
+rm_existing cdn_urls_input.txt 
+rm_existing cdn_urls_missing.csv  # Contains missing CDN objects from CCLW
+rm_existing cdn_urls_results.txt
 
 
 echo "--------------------------------------------------------------------------------"
@@ -38,26 +42,39 @@ do
   id=$(echo $line | cut -d '|' -f2)
   md5=$(echo $line | cut -d '|' -f3)
 
+  echo -n "${id}, ${md5}, "  >> cdn_urls_results.txt
+
   short="${url:0:24}…${url: -30}"
   echo -ne "${id}  ${short}... checking url                                   \r"
   found=$(curl -I -s $url | grep "HTTP/2 302" >/dev/null && echo "found" || echo "missing")
   if [ ${found} == "missing" ]
   then
-    echo "${id}, Missing url: ${url}"  >> cdn_urls_results.txt
+    echo "Missing url: ${url}"  >> cdn_urls_results.txt
     continue
-  else
-    # Now check MD5 
-    tmp=/tmp/tmp-cclw-${id}
-    echo -ne "${id}  ${short}... checking md5                                   \r"
+  fi
+
+  # Now check MD5 
+  if [ "x${md5}" == "x" ]
+  then
+    echo "Missing md5 skipping" >> cdn_urls_results.txt
+    continue
+  fi
+
+  # Now check MD5 against download
+  tmp=/tmp/tmp-cclw-${id}
+  echo -ne "${id}  ${short}... checking md5                                   \r"
+  # if the file has not already been downloaded
+  if [ ! -f ${tmp} ]
+  then
     curl -kLs ${url} --output ${tmp}
-    md5_found=$(md5sum ${tmp} | cut -d ' ' -f1)
-    if [ ! ${md5} == ${md5_found} ]
-    then
-      echo "MISMATCH: ${id},${md5},${md5_found}"
-      echo "${id}, Mismatch md5: ${md5_found}"  >> cdn_urls_results.txt
-    else
-      echo "${id}, OK"  >> cdn_urls_results.txt
-    fi
+  fi
+
+  md5_found=$(md5sum ${tmp} | cut -d ' ' -f1)
+  if [ ! ${md5} == ${md5_found} ]
+  then
+    echo "Mismatch md5: ${md5_found}"  >> cdn_urls_results.txt
+  else
+    echo "OK"  >> cdn_urls_results.txt
   fi
 done
 
