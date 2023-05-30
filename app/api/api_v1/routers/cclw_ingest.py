aba0a5f0cc32b34f1028b8ee3971de4d55157a7c
Peter Hooper <diversemix@gmail.com>
2023-05-30 17:19:39 +0100

    Add a test for the import_ids (#122)
    * Add a test for the import_ids
    
    * fix test and add negative one
    
    * Tighten ID_MATCHER, remove redundant code, ensure logging works

--- app/api/api_v1/routers/cclw_ingest.py
+++ app/api/api_v1/routers/cclw_ingest.py
@@ -12,12 +12,10 @@ from fastapi import (
     UploadFile,
     status,
 )
-from sqlalchemy import update
 from app.core.aws import S3Document
 
 from app.api.api_v1.schemas.document import (
     BulkIngestResult,
-    DocumentUpdateRequest,
 )
 from app.core.auth import get_superuser_details
 from app.core.aws import get_s3_client
@@ -41,7 +39,6 @@ from app.core.ingestion.utils import (
     get_result_counts,
 )
 from app.core.ingestion.validator import validate_event_row
-from app.core.validation import IMPORT_ID_MATCHER
 from app.core.validation.types import ImportSchemaMismatchError
 from app.core.validation.util import (
     get_new_s3_prefix,
@@ -49,8 +46,6 @@ from app.core.validation.util import (
     write_documents_to_s3,
     write_ingest_results_to_s3,
 )
-from app.db.models.document.physical_document import PhysicalDocument
-from app.db.models.law_policy.family import FamilyDocument, Slug
 from app.db.session import get_db
 
 _LOGGER = logging.getLogger(__name__)
@@ -397,88 +392,3 @@ def _validate_cclw_csv(
     _LOGGER.info(message)
 
     return documents_file_contents, message
-
-
-@r.put("/documents/{import_id_or_slug}", status_code=status.HTTP_200_OK)
-async def update_document(
-    request: Request,
-    import_id_or_slug: str,
-    meta_data: DocumentUpdateRequest,
-    db=Depends(get_db),
-    current_user=Depends(get_superuser_details),
-):
-    # TODO: As this grows move it out into the crud later.
-
-    _LOGGER.info(
-        f"Superuser '{current_user.email}' called update_document",
-        extra={
-            "props": {
-                "superuser_email": current_user.email,
-                "import_id_or_slug": import_id_or_slug,
-                "meta_data": meta_data,
-            }
-        },
-    )
-
-    # First query the FamilyDocument
-    query = db.query(FamilyDocument)
-    if IMPORT_ID_MATCHER.match(import_id_or_slug) is not None:
-        family_document = query.filter(
-            FamilyDocument.import_id == import_id_or_slug
-        ).one_or_none()
-        _LOGGER.info("update_document called with import_id")
-    else:
-        family_document = (
-            query.join(Slug, Slug.family_document_import_id == FamilyDocument.import_id)
-            .filter(Slug.name == import_id_or_slug)
-            .one_or_none()
-        )
-        _LOGGER.info("update_document called with slug")
-
-    # Check we have found one
-    if family_document is None:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        )
-
-    # Get the physical document to update
-    physical_document = family_document.physical_document
-
-    # Note this code relies on the fields being the same as the db column names
-    num_changed = db.execute(
-        update(PhysicalDocument)
-        .values(meta_data.dict())
-        .where(PhysicalDocument.id == physical_document.id)
-    ).rowcount
-
-    if num_changed == 0:
-        _LOGGER.info("update_document complete - nothing changed")
-        return physical_document  # Nothing to do - as should be idempotent
-
-    if num_changed > 1:
-        # This should never happen due to table uniqueness constraints
-        # TODO Rollback
-        raise HTTPException(
-            detail=(
-                f"There was more than one document identified by {import_id_or_slug}. "
-                "This should not happen!!!"
-            ),
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        )
-
-    db.commit()
-    db.refresh(physical_document)
-    _LOGGER.info(
-        "Call to update_document complete",
-        extra={
-            "props": {
-                "superuser_email": current_user.email,
-                "num_changed": num_changed,
-                "import_id": family_document.import_id,
-                "md5_sum": physical_document.md5_sum,
-                "content_type": physical_document.content_type,
-                "cdn_object": physical_document.cdn_object,
-            }
-        },
-    )
-    return physical_document
