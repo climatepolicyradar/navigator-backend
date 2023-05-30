6aa2c390fabfbb70bde5330260ce69833fc1e530
Joel Wright <joel@climatepolicyradar.org>
2023-05-30 15:30:54 +0100

    Add document & language metadata to config endpoint

--- app/core/lookups.py
+++ app/core/lookups.py
@@ -6,18 +6,39 @@ from app.core.organisation import get_organisation_taxonomy_by_name
 
 from app.core.util import tree_table_to_json
 from app.db.models.app.users import Organisation
-from app.db.models.law_policy import Geography
+from app.db.models.document.physical_document import Language
+from app.db.models.law_policy import (
+    Geography,
+    FamilyDocumentRole,
+    FamilyDocumentType,
+    Variant,
+)
 
 
 def get_config(db: Session) -> ApplicationConfig:
-    org_names = db.query(Organisation.name).all()
-
     return ApplicationConfig(
         geographies=tree_table_to_json(table=Geography, db=db),
         taxonomies={
-            org_name[0]: get_organisation_taxonomy_by_name(db=db, org_name=org_name[0])
-            for org_name in org_names
+            org.name: get_organisation_taxonomy_by_name(db=db, org_name=org.name)
+            for org in db.query(Organisation).all()
         },
+        languages={lang.language_code: lang.name for lang in db.query(Language).all()},
+        document_roles=[
+            doc_role.name
+            for doc_role in db.query(FamilyDocumentRole)
+            .order_by(FamilyDocumentRole.name)
+            .all()
+        ],
+        document_types=[
+            doc_type.name
+            for doc_type in db.query(FamilyDocumentType)
+            .order_by(FamilyDocumentType.name)
+            .all()
+        ],
+        document_variants=[
+            variant.variant_name
+            for variant in db.query(Variant).order_by(Variant.variant_name).all()
+        ],
     )
 
 
