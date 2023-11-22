f7406d5534056cd9b6b107344f260ea3748ce24b
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2023-11-22 12:24:38 +0000

    Added tests for testing created/last modified timestamps for independent family and document entities.

--- tests/core/ingestion/test_family.py
+++ tests/core/ingestion/test_family.py
@@ -58,34 +58,38 @@ def test_family_from_row__creates(test_db: Session):
     assert family == new_family
     assert new_family.published_date is None
     assert new_family.last_updated_date is None
+    assert new_family.created is not None
+    assert new_family.last_modified is not None
+    assert new_family.created == new_family.last_modified
 
 
-def test_family_from_row__updates(test_db: Session):
+def test_family_from_row__updates(test_db: Session, patch_current_time):
     populate_for_ingest(test_db)
-    result = {}
-    row = CCLWDocumentIngestRow.from_row(1, get_doc_ingest_row_data(0))
-    # Pre-Add the family
-    category = FamilyCategory(row.category.upper())
-    test_db.add(
-        Family(
-            import_id=FAMILY_IMPORT_ID,
-            title=row.family_name,
-            geography_id=2,
-            description=row.family_summary,
-            family_category=category,
+    with patch_current_time("2000-01-01 00:00:00.0Z", Family):
+        result = {}
+        row = CCLWDocumentIngestRow.from_row(1, get_doc_ingest_row_data(0))
+        # Pre-Add the family
+        category = FamilyCategory(row.category.upper())
+        test_db.add(
+            Family(
+                import_id=FAMILY_IMPORT_ID,
+                title=row.family_name,
+                geography_id=2,
+                description=row.family_summary,
+                family_category=category,
+            )
         )
-    )
-    pd = create_physical_document_from_params(
-        test_db, build_params_from_cclw(row), result
-    )
-    fd = FamilyDocument(
-        family_import_id=FAMILY_IMPORT_ID,
-        physical_document_id=pd.id,
-        import_id=DOCUMENT_IMPORT_ID,
-        document_status=DocumentStatus.CREATED,
-    )
-    test_db.add(fd)
-    add_a_slug_for_family1_and_flush(test_db)
+        pd = create_physical_document_from_params(
+            test_db, build_params_from_cclw(row), result
+        )
+        fd = FamilyDocument(
+            family_import_id=FAMILY_IMPORT_ID,
+            physical_document_id=pd.id,
+            import_id=DOCUMENT_IMPORT_ID,
+            document_status=DocumentStatus.CREATED,
+        )
+        test_db.add(fd)
+        add_a_slug_for_family1_and_flush(test_db)
     test_db.query(Family).filter(Family.import_id == FAMILY_IMPORT_ID).one()
     test_db.flush()
 
@@ -97,6 +101,10 @@ def test_family_from_row__updates(test_db: Session):
 
     assert family.title == "cheese"
     assert 1 == test_db.query(Family).filter_by(title="cheese").count()
+    assert family.created is not None
+    assert family.created == datetime(2000, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
+    assert family.last_modified is not None
+    assert family.created < family.last_modified
 
 
 def test_family_document_from_row__creates(test_db: Session):
@@ -131,11 +139,10 @@ def test_family_document_from_row__creates(test_db: Session):
 
 
 def test_family_document_from_row__updates(test_db: Session, patch_current_time):
-    with patch_current_time("2000-01-01 00:00:00.0Z", tick=False):
-        populate_for_ingest(test_db)
-
-        row = CCLWDocumentIngestRow.from_row(1, get_doc_ingest_row_data(0))
+    populate_for_ingest(test_db)
+    with patch_current_time("2000-01-01 00:00:00.0Z", FamilyDocument):
         family = add_a_family(test_db)
+        row = CCLWDocumentIngestRow.from_row(1, get_doc_ingest_row_data(0))
         result = {}
         handle_family_document_from_params(
             test_db, build_params_from_cclw(row), family, result=result
@@ -153,7 +160,6 @@ def test_family_document_from_row__updates(test_db: Session, patch_current_time)
         2000, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc
     )
     assert family_document.last_modified is not None
-    assert family_document.created != family_document.last_modified
     assert family_document.created < family_document.last_modified
 
     actual_keys = set(result.keys())
@@ -173,18 +179,16 @@ def test_family_document_from_row__updates(test_db: Session, patch_current_time)
     assert db_family_doc == family_document
     assert db_family_doc.physical_document.title == "test-title"
     assert db_family_doc.document_role == "PRESS RELEASE"
-
     assert db_family_doc.created == datetime(
         2000, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc
     )
     assert db_family_doc.last_modified is not None
-    assert db_family_doc.created != db_family_doc.last_modified
     assert db_family_doc.created < db_family_doc.last_modified
 
 
 def test_family_document_from_row__updates_status(test_db: Session, patch_current_time):
-    with patch_current_time("2000-01-01 00:00:00.0Z", tick=False):
-        populate_for_ingest(test_db)
+    populate_for_ingest(test_db)
+    with patch_current_time("2000-01-01 00:00:00.0Z", FamilyDocument):
         row = CCLWDocumentIngestRow.from_row(1, get_doc_ingest_row_data(0))
         family = add_a_family(test_db)
         result = {}
@@ -211,7 +215,6 @@ def test_family_document_from_row__updates_status(test_db: Session, patch_curren
         2000, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc
     )
     assert db_family_doc.last_modified is not None
-    assert db_family_doc.created != db_family_doc.last_modified
     assert db_family_doc.created < db_family_doc.last_modified
 
 
