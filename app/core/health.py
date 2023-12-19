d15f5e894caf96d29c1e3d1a39fef216b7c52baa
olaughter <51889566+olaughter@users.noreply.github.com>
2023-12-19 11:30:01 +0000

    Add postgres and vespa healthchecks (#204)
    * Add details on health check failures
    
    Previously this just silently retried until it hit a timeout, meaning
    longer delays on identifying issues. As we are adding actual healthchecks
    its worth having some feedback on whether there are actually any issues.
    
    * Add vespa & postgres healthchecks
    
    Having the backend lose connection to postgres or vespa would be bad.
    This adds health checks for both into the main app health check.
    
    For postgres this takes inspiration from the admin service' implementation,
    which can be found here: https://github.com/climatepolicyradar/navigator-admin-backend/blob/main/app/service/health.py#L11
    
    For vespa it makes use of the built in health endpoint for each instance,
    details here: https://docs.vespa.ai/en/reference/healthchecks.html
    
    * Discard stdout for healthcheck
    
    This also switches to using the container name rather than localhost
    
    * Update VESPA_URL env example
    
    For local development this should be the docker service we spin up with
    docker-compose, for deployed services it should be an actual vespa cloud
    instance endpoint

--- app/core/health.py
+++ app/core/health.py
@@ -1,13 +1,26 @@
 from fastapi import Depends
+import requests
+from requests.compat import urljoin
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
+from app.core.config import VESPA_URL
+
+
+VESPA_HEALTH_ENDPOINT = urljoin(VESPA_URL, "/status.html")
+
+
+def is_rds_online(db):
+    """Runs a sql query against the db"""
+    return db.execute("SELECT 1") is not None
+
+
+def is_vespa_online():
+    """Checks the health endpoint for the deployed vespa instance"""
+    response = requests.get(VESPA_HEALTH_ENDPOINT)
+    return response.ok
 
 
 def is_database_online(db: Session = Depends(get_db)) -> bool:
-    """
-    Checks database health.
-
-    TODO: More comprehensive health checks
-    """
-    return True
+    """Checks database health."""
+    return all([is_rds_online(db), is_vespa_online()])
