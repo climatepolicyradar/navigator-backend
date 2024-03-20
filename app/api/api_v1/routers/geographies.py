a6ece3e8cd19831ef98046b7e69f253315165506
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-03-20 10:15:39 +0000

    Raise HTTP exceptions on error or no stats found

--- app/api/api_v1/routers/geographies.py
+++ app/api/api_v1/routers/geographies.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.api.api_v1.schemas.geography import GeographyDTO
 from app.db.crud.geography import get_geography_stats
@@ -22,11 +22,10 @@ async def geographies(db=Depends(get_db)):
 
         if geo_stats == []:
             _LOGGER.error("No geo stats found")
-            return []
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="No geo stats found"
+            )
 
         return geo_stats
     except Exception as e:
-        # raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=e)
-        _LOGGER.error(e)
-
-    return []
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=e)
