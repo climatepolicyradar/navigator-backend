bce433d5b9003031c5fe1bea461ddcd92c68fbeb
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-04-08 17:36:27 +0100

    Remove unnecessary double quotes

--- startup.sh
+++ startup.sh
@@ -7,13 +7,13 @@ VESPA_KEY="${VESPA_KEY-}"
 VESPA_SECRETS_LOCATION="${VESPA_SECRETS_LOCATION:-/secrets}"
 
 # base64 decode key/cert supplied in env vars
-if [[ -n "${VESPA_CERT}" ]]; then
+if [[ -n ${VESPA_CERT} ]]; then
 	echo "${VESPA_CERT}" | openssl base64 -A -d >"${VESPA_SECRETS_LOCATION}/cert.pem"
 else
 	echo "No Vespa certificate supplied, skipping file creation"
 fi
 
-if [[ -n "${VESPA_KEY}" ]]; then
+if [[ -n ${VESPA_KEY} ]]; then
 	echo "${VESPA_KEY}" | openssl base64 -A -d >"${VESPA_SECRETS_LOCATION}/key.pem"
 else
 	echo "No Vespa key supplied, skipping file creation"
