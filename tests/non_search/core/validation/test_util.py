1cf73c89bd68affdc305a8d3ba2e9530c950836a
olaughter <fred@climatepolicyradar.org>
2024-05-22 13:52:44 +0100

    Move PIPELINE_BUCKET to config

--- tests/non_search/core/validation/test_util.py
+++ tests/non_search/core/validation/test_util.py
@@ -6,7 +6,8 @@ from unittest import mock
 import pytest
 
 from app.api.api_v1.schemas.document import DocumentParserInput
-from app.core.validation import IMPORT_ID_MATCHER, PIPELINE_BUCKET
+from app.core.config import PIPELINE_BUCKET
+from app.core.validation import IMPORT_ID_MATCHER
 from app.core.validation.util import _flatten_maybe_tree, write_documents_to_s3
 
 NOT_A_TREE_1 = [{"name": 1}, {"name": 2}, {"name": 3}]
