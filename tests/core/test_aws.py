d2232c77597e94c23bdf67e2cfd29bcc37c1f707
olaughter <fred@climatepolicyradar.org>
2024-06-04 16:11:38 +0100

    Consolidate s3 fixtures

--- tests/core/test_aws.py
+++ tests/core/test_aws.py
@@ -1,9 +1,5 @@
-from unittest.mock import patch
-
-from app.core.aws import S3Client
-
-
-def test_s3client_get_latest_ingest_start(mock_pipeline_bucket):
-    with patch("app.core.aws.PIPELINE_BUCKET", mock_pipeline_bucket):
-        start_date = S3Client(False).get_latest_ingest_start()
-    assert start_date == "2024-01-02"
+def test_s3client_get_latest_ingest_start(test_s3_client):
+    start_date = test_s3_client.get_latest_ingest_start(
+        pipeline_bucket="test_pipeline_bucket", ingest_trigger_root="input"
+    )
+    assert start_date == "2024-03-22"
