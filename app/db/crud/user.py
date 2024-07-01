794b44bdbe6f89d206f49331b433f7e48e789c9d
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-07-01 14:18:10 +0100

    Use tuple instead of Tuple

--- app/db/crud/user.py
+++ app/db/crud/user.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 from db_client.models.organisation import AppUser, Organisation, OrganisationUser
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
@@ -14,7 +12,7 @@ def get_app_user_by_email(db: Session, email: str) -> AppUser:
 
 def get_app_user_authorisation(
     db: Session, app_user: AppUser
-) -> list[Tuple[OrganisationUser, Organisation]]:
+) -> list[tuple[OrganisationUser, Organisation]]:
     return (
         db.query(OrganisationUser, Organisation)
         .filter(OrganisationUser.appuser_email == app_user.email)
