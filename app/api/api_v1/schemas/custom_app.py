3824be3b70e5d8ec3c6e3cbb6e5812f93a2c42bc
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-08-07 17:20:32 +0100

    Add DTO for custom app configuration model

---
+++ app/api/api_v1/schemas/custom_app.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class CustomAppConfigDTO(BaseModel):
+    """A JSON representation of custom app configurable options."""
+
+    allowed_corpora_ids: list[str]
+    years: Optional[int]
+    exp: int
+    iat: int
