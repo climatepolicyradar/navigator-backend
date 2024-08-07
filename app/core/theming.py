a1b74b3c080476d3f56b2638ea53dca33e14f3b4
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-08-07 13:51:11 +0100

    Add docstrings & logging & remove validation for now

--- app/core/theming.py
+++ app/core/theming.py
@@ -1,26 +1,38 @@
+import logging
 import os
 
+import jwt
 from db_client.models.dfce.family import Corpus
 from sqlalchemy.orm import Session
 
-from app.db.session import get_db
+_LOGGER = logging.getLogger(__name__)
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 ALGORITHM = "HS256"
 
 
 def validate(db: Session, allowed_corpora_ids: list[str]) -> bool:
-    """Validate whether a corpus with the given ID exists in the DB.
+    """Validate whether all given corpus IDs exist in the DB.
 
     :param Session db: The DB session to connect to.
-    :param str corpus_id: The corpus import ID we want to validate.
-    :return bool: Return whether or not the corpus exists in the DB.
+    :param list[str] allowed_corpora_ids: The corpus import IDs we want
+        to validate.
+    :return bool: Return whether or not all the corpora exist in the DB.
     """
     existing_corpora_in_db = db.query(Corpus.import_id).distinct().all()
     return all(corpus in existing_corpora_in_db for corpus in allowed_corpora_ids)
 
 
-def create_configuration_token(allowed_corpora: str):
-    db = next(get_db())
+def create_configuration_token(allowed_corpora: str) -> str:
+    """Create a custom app configuration token.
+
+    :param str allowed_corpora: A comma separated string containing the
+        corpus import IDs that the custom app should show.
+    :return str: A JWT token containing the encoded allowed corpora.
+    """
     corpora_ids = allowed_corpora.split(",")
-    print(validate(db, corpora_ids))
+    msg = "Creating custom app configuration token for the following corpora:"
+    msg += f" {corpora_ids}"
+    _LOGGER.info(msg)
+    to_encode = {"allowed_corpora_ids": corpora_ids}
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
