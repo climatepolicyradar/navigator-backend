68827c9ae2c998f05a450ab264ce12e739f8f085
Mark <mark@climatepolicyradar.org>
2024-09-12 16:11:43 +0100

    Removing from test.

--- tests/search/test_vespasearch.py
+++ tests/search/test_vespasearch.py
@@ -842,7 +842,6 @@ def test_csv_content(
         assert row["Family Publication Date"] == family["family_date"]
         assert row["Category"] == family["family_category"]
         assert row["Geography"] == family["family_geography"]
-        assert row["Geography"] in family["family_geographies"]
 
         # TODO: Add collections to test db setup to provide document level coverage
 
