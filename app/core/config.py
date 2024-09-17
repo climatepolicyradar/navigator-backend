04c826911832c62cebffb49bea817118d2addf26
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-09-17 17:09:47 +0100

    Default VESPA_SECRETS_LOCATION to None if not found

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ import os
 from typing import Final, Optional
 
 PROJECT_NAME = "navigator"
-DEVELOPMENT_MODE: bool = os.getenv("DEVELOPMENT_MODE", "False").lower() == "true"
+
 
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "")
 if not SQLALCHEMY_DATABASE_URI:
@@ -12,9 +12,7 @@ PUBLIC_APP_URL = os.environ["PUBLIC_APP_URL"].rstrip("/")
 API_V1_STR = "/api/v1"
 
 # Vespa Config
-VESPA_SECRETS_LOCATION: Optional[str] = (
-    None if DEVELOPMENT_MODE else os.getenv("VESPA_SECRETS_LOCATION", "/secrets")
-)
+VESPA_SECRETS_LOCATION: Optional[str] = os.getenv("VESPA_SECRETS_LOCATION", None)
 VESPA_URL: str = os.environ["VESPA_URL"]
 
 # Shared search config
@@ -25,5 +23,6 @@ INGEST_CYCLE_START = os.getenv("INGEST_CYCLE_START")
 DOC_CACHE_BUCKET = os.getenv("DOCUMENT_CACHE_BUCKET")
 PIPELINE_BUCKET: str = os.getenv("PIPELINE_BUCKET", "not_set")
 INGEST_TRIGGER_ROOT: Final = "input"
+DEVELOPMENT_MODE: bool = os.getenv("DEVELOPMENT_MODE", "False").lower() == "true"
 AWS_REGION = os.getenv("AWS_REGION", "eu-west-1")
 CDN_DOMAIN = os.getenv("CDN_DOMAIN")
