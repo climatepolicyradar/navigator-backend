975286f20840f77047124e6ad73e195312c4e86e
diversemix <diversemix@gmail.com>
2024-10-02 15:25:34 +0100

    Add tests for multi geog

--- tests/unit/app/db/crud/test_get_family_document_and_context.py
+++ tests/unit/app/db/crud/test_get_family_document_and_context.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from db_client.models.dfce import FamilyGeography
 from sqlalchemy.orm import Session
 
 from app.api.api_v1.schemas.document import FamilyDocumentWithContextResponse
@@ -12,6 +13,27 @@ def test_get_family_document_and_context(
     data_db: Session,
 ):
     setup_with_documents_large_with_families(documents_large, data_db)
+    response: FamilyDocumentWithContextResponse = get_family_document_and_context(
+        data_db, "CCLW.document.i00000192.n0000"
+    )
+
+    assert response.family.import_id == "CCLW.family.1001.0"
+    assert response.document.import_id == "CCLW.document.i00000192.n0000"
+
+
+def test_get_family_document_and_context_extra_geog(
+    documents_large: list[Dict],
+    data_db: Session,
+):
+    setup_with_documents_large_with_families(documents_large, data_db)
+    # add an extra geography
+    data_db.add(
+        FamilyGeography(
+            family_import_id="CCLW.family.1001.0",
+            geography_id=2,
+        )
+    )
+    data_db.commit()
     response: FamilyDocumentWithContextResponse = get_family_document_and_context(
         data_db, "CCLW.document.i00000192.n0000"
     )
