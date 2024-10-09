6234aa8a304e5c361510e1181ae940b5a148ef45
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-10-09 09:20:21 +0100

    Remove fixme

--- app/api/api_v1/routers/search.py
+++ app/api/api_v1/routers/search.py
@@ -304,8 +304,6 @@ def download_all_search_documents(
         # upload a buffer, it starts from the position it is currently in. We need to
         # add the seek(0) to reset the buffer position to the beginning before writing
         # to S3 to avoid creating an empty file.
-        #
-        # FIXME Add where clause to filter out MCF data
         zip_buffer = create_data_download_zip_archive(
             latest_ingest_start, token.allowed_corpora_ids, db
         )
