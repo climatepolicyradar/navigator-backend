167c40af9d0ba4b22fb7843ae968b5606533c88f
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-10-14 10:57:13 +0100

    Sort imports

--- app/api/api_v1/routers/pipeline_trigger.py
+++ app/api/api_v1/routers/pipeline_trigger.py
@@ -4,11 +4,11 @@ from fastapi import APIRouter, BackgroundTasks, Depends, Request, status
 from sqlalchemy.orm import Session
 
 from app.api.api_v1.schemas.document import BulkIngestResult
+from app.clients.aws.client import S3Client, get_s3_client
 from app.clients.db.session import get_db
 from app.core.ingestion.pipeline import generate_pipeline_ingest_input
 from app.core.validation.util import get_new_s3_prefix, write_documents_to_s3
 from app.service.auth import get_superuser_details
-from app.clients.aws.client import S3Client, get_s3_client
 
 _LOGGER = logging.getLogger(__name__)
 
