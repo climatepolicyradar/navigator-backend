10b9dec1678c1dc4e9b63adadda511ceb00fa1be
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-11-28 15:02:52 +0000

    Update world map stats repo function

--- app/repository/geography.py
+++ app/repository/geography.py
@@ -1,22 +1,19 @@
 """Functions to support the geographies endpoint."""
 
 import logging
+import os
 from typing import Optional, Sequence
 
-from db_client.models.dfce.family import (
-    Family,
-    FamilyDocument,
-    FamilyGeography,
-    FamilyStatus,
-)
+from db_client.models.dfce.family import Family, FamilyDocument, FamilyGeography
 from db_client.models.dfce.geography import Geography
-from sqlalchemy import func
-from sqlalchemy.dialects import postgresql
+from sqlalchemy import bindparam, text
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Query, Session
+from sqlalchemy.types import ARRAY, String
 
-from app.errors import RepositoryError
+from app.errors import RepositoryError, ValidationError
 from app.models.geography import GeographyStatsDTO
+from app.repository.helpers import get_query_template
 
 _LOGGER = logging.getLogger(__file__)
 
@@ -66,85 +63,30 @@ def get_geo_subquery(
 
 def _db_count_fams_in_category_and_geo(
     db: Session, allowed_corpora: Optional[list[str]]
-) -> Query:
-    """
-    Query the database for the fam count per category per geo.
-
-    NOTE: SqlAlchemy will make a complete hash of query generation if
-    columns are used in the query() call. Therefore, entire objects are
-    returned.
+) -> list[GeographyStatsDTO]:
+    """Query the database for the family count per category per geo.
 
     :param Session db: DB Session to perform query on.
     :param Optional[list[str]] allowed_corpora: The list of allowed
         corpora IDs to filter on.
-    :return Query: A Query object containing the queries to perform.
+    :return list[GeographyStatsDTO]: A list of counts of families by
+        category per geography.
     """
-    # Get the required Geography information and cross join each with all of the unique
-    # family_category values (so if some geographies have no documents for a particular
-    # family_category, we can set the count for that category to 0).
-    family_categories = db.query(Family.family_category).distinct().subquery()
-    geo_family_combinations = db.query(
-        Geography.id.label("geography_id"),
-        Geography.display_value,
-        Geography.slug,
-        Geography.value,
-        family_categories.c.family_category,
-    ).subquery("geo_family_combinations")
+    if allowed_corpora in [None, []]:
+        raise ValidationError("No allowed corpora provided")
 
-    # Get a count of documents in each present family_category for each geography.
-    counts = (
-        db.query(
-            Family.family_category,
-            FamilyGeography.geography_id,
-            func.count().label("records_count"),
-        )
-        .join(FamilyGeography, Family.import_id == FamilyGeography.family_import_id)
-        # .join(FamilyCorpus, Family.import_id == FamilyCorpus.family_import_id)
-        # .join(Corpus, Corpus.import_id == FamilyCorpus.family_import_id)
-        .filter(Family.family_status == FamilyStatus.PUBLISHED)
-        .group_by(Family.family_category, FamilyGeography.geography_id)
-        .subquery("counts")
+    query_template = text(
+        get_query_template(os.path.join("app", "repository", "sql", "world_map.sql"))
     )
-    # if allowed_corpora is not None:
-    #     counts = counts.where(Corpus.import_id.in_(allowed_corpora))
-    # counts = counts.group_by(
-    #     Family.family_category, FamilyGeography.geography_id
-    # ).subquery("counts")
-    # _LOGGER.info(counts)
+    query_template = query_template.bindparams(
+        bindparam("allowed_corpora_ids", value=allowed_corpora, type_=ARRAY(String)),
+    )
 
-    # Aggregate family_category counts per geography into a JSONB object, and if a
-    # family_category count is missing, set the count for that category to 0 so each
-    # geography will always have a count for all family_category values.
-    query = (
-        db.query(
-            geo_family_combinations.c.display_value.label("display_value"),
-            geo_family_combinations.c.slug.label("slug"),
-            geo_family_combinations.c.value.label("value"),
-            func.jsonb_object_agg(
-                geo_family_combinations.c.family_category,
-                func.coalesce(counts.c.records_count, 0),
-            ).label("counts"),
-        )
-        .select_from(
-            geo_family_combinations.join(
-                counts,
-                (geo_family_combinations.c.geography_id == counts.c.geography_id)
-                & (
-                    geo_family_combinations.c.family_category
-                    == counts.c.family_category
-                ),
-                isouter=True,
-            )
-        )
-        .group_by(
-            geo_family_combinations.c.display_value,
-            geo_family_combinations.c.slug,
-            geo_family_combinations.c.value,
-        )
-        .order_by(geo_family_combinations.c.display_value)
-    )
-    print(query.statement.compile(dialect=postgresql.dialect()))
-    return query
+    family_geo_stats = db.execute(
+        query_template, {"allowed_corpora_ids": allowed_corpora}
+    ).all()
+    results = [_to_dto(fgs) for fgs in family_geo_stats]
+    return results
 
 
 def _to_dto(family_doc_geo_stats) -> GeographyStatsDTO:
@@ -176,13 +118,11 @@ def get_world_map_stats(
     :return list[GeographyStatsDTO]: A list of Geography stats objects
     """
     try:
-        family_geo_stats = _db_count_fams_in_category_and_geo(db, allowed_corpora).all()
+        family_geo_stats = _db_count_fams_in_category_and_geo(db, allowed_corpora)
     except OperationalError as e:
         _LOGGER.error(e)
         raise RepositoryError("Error querying the database for geography stats")
 
     if not family_geo_stats:
         return []
-
-    result = [_to_dto(fgs) for fgs in family_geo_stats]
-    return result
+    return family_geo_stats
