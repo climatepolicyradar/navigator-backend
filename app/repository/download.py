6d9a219442439a25e473065e3b3feb52386edf48
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-11-28 13:33:41 +0000

    Update download query logic based on linting

--- app/repository/download.py
+++ app/repository/download.py
@@ -5,6 +5,8 @@ from logging import getLogger
 
 import pandas as pd
 from fastapi import Depends
+from sqlalchemy import bindparam, text
+from sqlalchemy.types import ARRAY, DATETIME, String
 
 from app.clients.db.session import get_db
 from app.repository.helpers import get_query_template
@@ -15,24 +17,31 @@ _LOGGER = getLogger(__name__)
 def get_whole_database_dump(
     ingest_cycle_start: str, allowed_corpora_ids: list[str], db=Depends(get_db)
 ):
-    """Get  whole database dump and bind variables.
+    """Get whole database dump and bind variables.
 
     :param str ingest_cycle_start: The current ingest cycle date.
     :param list[str] corpora_ids: The corpora from which we
         should allow the data to be dumped.
-    :return str: The SQL query to perform on the database session.
+    :return pd.DataFrame: A DataFrame containing the results of the SQL
+        query that gets the whole database dump in our desired format.
     """
-    query_template = get_query_template(
-        os.path.join("app", "repository", "sql", "download.sql")
+    query = text(
+        get_query_template(os.path.join("app", "repository", "sql", "download.sql"))
+    ).bindparams(
+        bindparam("ingest_cycle_start", type_=DATETIME),
+        bindparam(
+            "allowed_corpora_ids", value=allowed_corpora_ids, type_=ARRAY(String)
+        ),
     )
 
     with db.connection() as conn:
-        df = pd.read_sql(
-            query_template,
-            conn.connection,
-            params={
+        result = conn.execute(
+            query,
+            {
                 "ingest_cycle_start": ingest_cycle_start,
-                "allowed_corpora_ids": tuple(allowed_corpora_ids),
+                "allowed_corpora_ids": allowed_corpora_ids,
             },
         )
+        columns = result.keys()
+        df = pd.DataFrame(result.fetchall(), columns=columns)
         return df
