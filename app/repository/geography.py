84800988649d4332de738a884fc4048dcbd422dd
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-11-28 13:59:33 +0000

    Merge in main

--- app/repository/geography.py
+++ app/repository/geography.py
@@ -4,9 +4,7 @@ import logging
 from typing import Optional, Sequence
 
 from db_client.models.dfce.family import (
-    Corpus,
     Family,
-    FamilyCorpus,
     FamilyDocument,
     FamilyGeography,
     FamilyStatus,
@@ -146,8 +144,6 @@ def _db_count_fams_in_category_and_geo(
         .order_by(geo_family_combinations.c.display_value)
     )
     print(query.statement.compile(dialect=postgresql.dialect()))
-    # print(str(query.statement.compile(compile_kwargs={"literal_binds": True})))
-    print(render_query(query, db))
     return query
 
 
@@ -190,50 +186,3 @@ def get_world_map_stats(
 
     result = [_to_dto(fgs) for fgs in family_geo_stats]
     return result
-
-
-from datetime import date, datetime, timedelta
-
-from sqlalchemy.orm import Query
-
-
-def render_query(statement, db_session):
-    """
-    Generate an SQL expression string with bound parameters rendered inline
-    for the given SQLAlchemy statement.
-    WARNING: This method of escaping is insecure, incomplete, and for debugging
-    purposes only. Executing SQL statements with inline-rendered user values is
-    extremely insecure.
-    Based on http://stackoverflow.com/questions/5631078/sqlalchemy-print-the-actual-query
-    """
-    if isinstance(statement, Query):
-        statement = statement.statement
-    dialect = db_session.bind.dialect
-
-    class LiteralCompiler(dialect.statement_compiler):
-        def visit_bindparam(
-            self, bindparam, within_columns_clause=False, literal_binds=False, **kwargs
-        ):
-            return self.render_literal_value(bindparam.value, bindparam.type)
-
-        def render_array_value(self, val, item_type):
-            if isinstance(val, list):
-                return "{}".format(
-                    ",".join([self.render_array_value(x, item_type) for x in val])
-                )
-            return self.render_literal_value(val, item_type)
-
-        def render_literal_value(self, value, type_):
-            if isinstance(value, int):
-                return str(value)
-            elif isinstance(value, (str, date, datetime, timedelta)):
-                return "'{}'".format(str(value).replace("'", "''"))
-            elif isinstance(value, list):
-                return "'{{{}}}'".format(
-                    ",".join(
-                        [self.render_array_value(x, type_.item_type) for x in value]
-                    )
-                )
-            return super(LiteralCompiler, self).render_literal_value(value, type_)
-
-    return LiteralCompiler(dialect, statement).process(statement)
--- app/repository/sql/world_map.sql
+++ app/repository/sql/world_map.sql
@@ -1,36 +1,13 @@
-SELECT
-  geo_family_combinations.display_value AS display_value,
-  geo_family_combinations.slug AS slug,
-  geo_family_combinations.value AS value,
-  jsonb_object_agg (
-    geo_family_combinations.family_category,
-    coalesce(counts.records_count, 0)
-  ) AS counts
-FROM
-  (
+WITH counts AS (
     SELECT
-      geography.id AS geography_id,
-      geography.display_value AS display_value,
-      geography.slug AS slug,
-      geography.value AS value,
-      anon_1.family_category AS family_category
-    FROM
-      geography,
-      (
-        SELECT DISTINCT
-          family.family_category AS family_category
-        FROM
-          family
-      ) AS anon_1
-  ) AS geo_family_combinations
-  LEFT OUTER JOIN (
-    SELECT
-      family.family_category AS family_category,
-      family_geography.geography_id AS geography_id,
-      count(*) AS records_count
+      family.family_category,
+      family_geography.geography_id,
+      COUNT(*) AS records_count
     FROM
       family
-      JOIN family_geography ON family.import_id = family_geography.family_import_id
+      INNER JOIN
+          family_geography
+          ON family.import_id = family_geography.family_import_id
     WHERE
       CASE
         WHEN (
@@ -45,11 +22,10 @@ FROM
             )
           )
         ) THEN 'Created'
-        ELSE CASE
-          WHEN (
+        WHEN (
             (
               SELECT
-                count(family_document.document_status) AS count_1
+                COUNT(family_document.document_status) AS count_1
               FROM
                 family_document
               WHERE
@@ -57,11 +33,10 @@ FROM
                 AND family_document.document_status = 'PUBLISHED'
             ) > 0
           ) THEN 'Published'
-          ELSE CASE
-            WHEN (
+        WHEN (
               (
                 SELECT
-                  count(family_document.document_status) AS count_2
+                  COUNT(family_document.document_status) AS count_2
                 FROM
                   family_document
                 WHERE
@@ -69,14 +44,41 @@ FROM
                   AND family_document.document_status = 'CREATED'
               ) > 0
             ) THEN 'Created'
-            ELSE 'Deleted'
-          END
-        END
+        ELSE 'Deleted'
       END = 'Published'
     GROUP BY
       family.family_category,
       family_geography.geography_id
-  ) AS counts ON geo_family_combinations.geography_id = counts.geography_id
+  )
+
+SELECT
+  geo_family_combinations.display_value,
+  geo_family_combinations.slug,
+  geo_family_combinations.value,
+  JSONB_OBJECT_AGG(
+    geo_family_combinations.family_category,
+    COALESCE(counts.records_count, 0)
+  ) AS counts
+FROM
+  (
+    SELECT
+      geography.id AS geography_id,
+      geography.display_value,
+      geography.slug,
+      geography.value,
+      anon_1.family_category
+    FROM
+      geography,
+      (
+        SELECT DISTINCT
+          family.family_category
+        FROM
+          family
+      ) AS anon_1
+  ) AS geo_family_combinations
+  LEFT OUTER JOIN
+      counts
+      ON geo_family_combinations.geography_id = counts.geography_id
   AND geo_family_combinations.family_category = counts.family_category
 GROUP BY
   geo_family_combinations.display_value,
