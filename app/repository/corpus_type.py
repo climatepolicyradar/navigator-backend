81948142e2cdadb8436c7d54fa55714fa5c83bef
Anna Pokorska <anna@climatepolicyradar.org>
2025-01-06 14:34:08 +0000

    Add missing docstring

--- app/repository/corpus_type.py
+++ app/repository/corpus_type.py
@@ -3,4 +3,11 @@ from sqlalchemy.orm import Session
 
 
 def get(db: Session, corpus_type_name: str) -> CorpusType:
+    """
+    Get a CorpusType object based on its name.
+
+    :param db: Database session
+    :param corpus_type_name: The name of the corpus type
+    :return: A CorpusType object
+    """
     return db.query(CorpusType).filter(CorpusType.name == corpus_type_name).one()
