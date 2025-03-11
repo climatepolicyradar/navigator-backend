373206ce74db02c73332512d27e98c3582d4948e
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-03-11 15:13:09 +0000

    fix: use correct key when accessing valid_metadata

--- tests/non_search/setup_helpers.py
+++ tests/non_search/setup_helpers.py
@@ -14,14 +14,14 @@ def get_default_collections():
         "import_id": "CPR.Collection.1.0",
         "title": "Collection1",
         "description": "CollectionSummary1",
-        "valid_metadata": {},
+        "metadata": {},
     }
 
     collection2 = {
         "import_id": "CPR.Collection.2.0",
         "title": "Collection2",
         "description": "CollectionSummary2",
-        "valid_metadata": {},
+        "metadata": {},
     }
     return collection1, collection2
 
