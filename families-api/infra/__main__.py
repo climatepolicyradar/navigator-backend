f1b845f2eae10450cffa9ee00d3badaa46bdc73c
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-05-01 16:47:49 +0100

    feat: pulumi secrets via SSM

--- families-api/infra/__main__.py
+++ families-api/infra/__main__.py
@@ -1,9 +1,105 @@
-"""An AWS Python Pulumi program"""
+import json
 
 import pulumi
 import pulumi_aws as aws
 
 account_id = aws.get_caller_identity().account_id
+
+
+def generate_secret_key(project: str, aws_service: str, name: str):
+    return f"/{project}/{aws_service}/{name}"
+
+
+# This stuff is being encapsulated in navigator-infra and we should use that once it is ready
+# IAM role trusted by App Runner
+families_api_role = aws.iam.Role(
+    "families-api-role",
+    assume_role_policy=json.dumps(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "build.apprunner.amazonaws.com"},
+                    "Action": "sts:AssumeRole",
+                }
+            ],
+        }
+    ),
+)
+
+# Attach ECR access policy to the role
+families_api_role_policy = aws.iam.RolePolicy(
+    "families-api-role-ecr-policy",
+    role=families_api_role.id,
+    policy=json.dumps(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "ecr:GetDownloadUrlForLayer",
+                        "ecr:BatchGetImage",
+                        "ecr:DescribeImages",
+                        "ecr:GetAuthorizationToken",
+                        "ecr:BatchCheckLayerAvailability",
+                    ],
+                    "Resource": "*",
+                }
+            ],
+        }
+    ),
+)
+
+families_api_instance_role = aws.iam.Role(
+    "families-api-instance-role",
+    assume_role_policy=json.dumps(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "tasks.apprunner.amazonaws.com"},
+                    "Action": "sts:AssumeRole",
+                }
+            ],
+        }
+    ),
+)
+
+# Allow access to specific SSM Parameter Store secrets
+families_api_ssm_policy = aws.iam.RolePolicy(
+    "families-api-instance-role-ssm-policy",
+    role=families_api_instance_role.id,
+    policy=json.dumps(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": ["ssm:GetParameters"],
+                    "Resource": [
+                        f"arn:aws:ssm:eu-west-1:{account_id}:parameter/families-api/apprunner/*"
+                    ],
+                }
+            ],
+        }
+    ),
+)
+
+families_api_apprunner_navigator_database_url = aws.ssm.Parameter(
+    "families-api-apprunner-navigator-database-url",
+    name=generate_secret_key("families-api", "apprunner", "NAVIGATOR_DATABASE_URL"),
+    description="The URL string to connect to the navigator database",
+    type=aws.ssm.ParameterType.SECURE_STRING,
+    # This value is managed directly in SSM
+    value="PLACEHOLDER",
+    opts=pulumi.ResourceOptions(
+        # This value is managed directly in SSM
+        ignore_changes=["value"]
+    ),
+)
 
 families_api_ecr_repository = aws.ecr.Repository(
     "families-api-ecr-repository",
@@ -20,6 +116,7 @@ families_api_ecr_repository = aws.ecr.Repository(
     opts=pulumi.ResourceOptions(protect=True),
 )
 
+
 families_api_apprunner_service = aws.apprunner.Service(
     "families-api-apprunner-service",
     auto_scaling_configuration_arn=f"arn:aws:apprunner:eu-west-1:{account_id}:autoscalingconfiguration/DefaultConfiguration/1/00000000000000000000000000000001",
@@ -27,6 +124,9 @@ families_api_apprunner_service = aws.apprunner.Service(
         "interval": 10,
         "protocol": "TCP",
         "timeout": 5,
+    },
+    instance_configuration={
+        "instance_role_arn": families_api_instance_role.arn,
     },
     network_configuration={
         "egress_configuration": {
@@ -43,12 +143,12 @@ families_api_apprunner_service = aws.apprunner.Service(
     service_name="families-api",
     source_configuration={
         "authentication_configuration": {
-            "access_role_arn": f"arn:aws:iam::{account_id}:role/production-backend-apprunner-access-role-642db82",
+            "access_role_arn": families_api_role.arn,
         },
         "image_repository": {
             "image_configuration": {
-                "runtime_environment_variables": {
-                    "NAVIGATOR_DATABASE_URL": "XXX",
+                "runtime_environment_secrets": {
+                    "NAVIGATOR_DATABASE_URL": families_api_apprunner_navigator_database_url.arn,
                 },
             },
             "image_identifier": f"{account_id}.dkr.ecr.eu-west-1.amazonaws.com/families-api:latest",
