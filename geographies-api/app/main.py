a279b985a615d52d152c5aca2e3da4eac9e1d047
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-16 15:43:55 +0100

    Add OTel to geographies-api

--- geographies-api/app/main.py
+++ geographies-api/app/main.py
@@ -1,10 +1,41 @@
+import logging
+import os
+from pathlib import Path
 from typing import TypeVar
 
+from api.telemetry import Telemetry
+from api.telemetry_config import ServiceManifest, TelemetryConfig
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.model import Settings
 from app.router import router as geographies_router
+
+_LOGGER = logging.getLogger(__name__)
+
+# we always use a path relative to the file as the calling process can come
+# from multiple locations
+root_dir = Path(__file__).parent.parent
+
+
+# Open Telemetry initialisation
+ENV = os.getenv("ENV", "development")
+os.environ["OTEL_PYTHON_LOG_CORRELATION"] = "True"
+try:
+    otel_config = TelemetryConfig.from_service_manifest(
+        ServiceManifest.from_file(f"{root_dir}/service-manifest.json"), ENV, "0.1.0"
+    )
+except Exception as _:
+    _LOGGER.error("Failed to load service manifest, using defaults")
+    otel_config = TelemetryConfig(
+        service_name="navigator-backend",
+        namespace_name="navigator",
+        service_version="0.0.0",
+        environment=ENV,
+    )
+
+telemetry = Telemetry(otel_config)
+tracer = telemetry.get_tracer()
 
 APIDataType = TypeVar("APIDataType")
 
@@ -58,3 +89,7 @@ app.include_router(
     tags=["Geographies"],
     include_in_schema=True,
 )
+
+# Open Telemetry instrumentation
+telemetry.instrument_fastapi(app)
+telemetry.setup_exception_hook()
