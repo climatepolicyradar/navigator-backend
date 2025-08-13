ba2f584a21a12990340fb3c73363baadd1a90178
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-08-13 15:27:38 +0100

    Create family repo

--- backend-api/app/api/api_v1/routers/families.py
+++ backend-api/app/api/api_v1/routers/families.py
@@ -5,7 +5,10 @@ from fastapi import APIRouter, Depends, Header, Request
 
 from app.clients.db.session import get_db
 from app.models.family import HomepageCountsResponse
-from app.repository.family import count_families_per_category_per_corpus
+from app.repository.family import (
+    count_families_per_category_per_corpus,
+    count_families_per_category_per_corpus_latest_ingest_cycle,
+)
 from app.service.custom_app import AppTokenFactory
 from app.telemetry_exceptions import ExceptionHandlingTelemetryRoute
 
@@ -23,3 +26,18 @@ def get_homepage_counts(
     token.decode_and_validate(db, request, app_token)
 
     return count_families_per_category_per_corpus(db, token.allowed_corpora_ids)
+
+
+@families_router.get(
+    "/homepage-counts-latest-ingest-cycle", response_model=HomepageCountsResponse
+)
+def get_homepage_counts_latest_ingest_cycle(
+    request: Request, app_token: Annotated[str, Header()], db=Depends(get_db)
+):
+    """Get the count of families by category per corpus for the homepage."""
+    token = AppTokenFactory()
+    token.decode_and_validate(db, request, app_token)
+
+    return count_families_per_category_per_corpus_latest_ingest_cycle(
+        db, token.allowed_corpora_ids
+    )
--- backend-api/app/repository/family.py
+++ backend-api/app/repository/family.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from db_client.models.dfce.family import (
     DocumentStatus,
     Family,
@@ -7,8 +9,42 @@ from db_client.models.dfce.family import (
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
+from app.service.util import get_latest_ingest_start
+
 
 def count_families_per_category_per_corpus(
+    db: Session, allowed_corpora_ids: Optional[list[str]] = None
+) -> list[tuple[str, int]]:
+    """
+    Get the count of families by category per corpus.
+
+    :param db: Database session
+    :param allowed_corpora_ids: The import IDs of the corpora
+    :return: A list of tuples where each tuple contains a family category and its count
+    """
+    # Subquery to find families with at least one published document
+    # Avoid using calculated family_status field for performance reasons
+    published_families = (
+        db.query(FamilyDocument.family_import_id)
+        .filter(FamilyDocument.document_status == DocumentStatus.PUBLISHED)
+        .distinct()
+        .subquery()
+    )
+
+    query = db.query(Family.family_category, func.count()).join(
+        published_families,
+        published_families.c.family_import_id == Family.import_id,
+    )
+
+    if allowed_corpora_ids is not None and allowed_corpora_ids:
+        query = query.join(
+            FamilyCorpus, FamilyCorpus.family_import_id == Family.import_id
+        ).filter(FamilyCorpus.corpus_import_id.in_(allowed_corpora_ids))
+
+    return query.group_by(Family.family_category).all()
+
+
+def count_families_per_category_per_corpus_latest_ingest_cycle(
     db: Session, allowed_corpora_ids: list[str]
 ) -> list[tuple[str, int]]:
     """
@@ -32,9 +68,13 @@ def count_families_per_category_per_corpus(
         published_families.c.family_import_id == Family.import_id,
     )
 
-    if allowed_corpora_ids is not None or allowed_corpora_ids != []:
+    if allowed_corpora_ids is not None and allowed_corpora_ids:
         query = query.join(
             FamilyCorpus, FamilyCorpus.family_import_id == Family.import_id
         ).filter(FamilyCorpus.corpus_import_id.in_(allowed_corpora_ids))
+
+    latest_ingest_start = get_latest_ingest_start()
+    if latest_ingest_start is not None:
+        query = query.filter(FamilyDocument.last_modified < latest_ingest_start)
 
     return query.group_by(Family.family_category).all()
