3f3478f4beeedd55fc8b1c23bada7f148f0eb5af
Osneil Drakes <osneildrakes@MacBookPro.communityfibre.co.uk>
2025-08-14 09:24:02 +0100

    trunk

--- backend-api/app/main.py
+++ backend-api/app/main.py
@@ -16,8 +16,8 @@ from app.api.api_v1.routers.admin import admin_document_router
 from app.api.api_v1.routers.auth import auth_router
 from app.api.api_v1.routers.collections import collections_router
 from app.api.api_v1.routers.documents import documents_router
-from app.api.api_v1.routers.latest_published import latest_published_router
 from app.api.api_v1.routers.families import families_router
+from app.api.api_v1.routers.latest_published import latest_published_router
 from app.api.api_v1.routers.lookups import lookups_router
 from app.api.api_v1.routers.pipeline_trigger import pipeline_trigger_router
 from app.api.api_v1.routers.search import search_router
