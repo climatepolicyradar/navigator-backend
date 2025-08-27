4ec52af34b4833b6a80dd6558e7ff163d07fd677
Henry <henry@climatepolicyradar.org>
2025-08-27 17:22:26 +0100

    Re-Revert main.py fix unneeded anymore

--- backend-api/app/clients/db/session.py
+++ backend-api/app/clients/db/session.py
@@ -5,6 +5,20 @@ from sqlalchemy.pool import NullPool
 
 from app import config
 
+"""
+Notes: August 27th 2025.
+
+We have been trying to trace segfault issues for months. They're 
+our white whale. We identified a hypothesis: the sqlalchemy engine
+and session were initialised on module import, before uvicorn 
+spawned the worker processes. This meant that the engine and session
+were shared across all workers. Ruh roh. SQLALCHEMY ISNT THREAD SAFE.
+
+The following code is a bit hacky, but ensures the engine is lazily
+initialised and therefore after uvicorn has spawned the worker 
+processes.
+"""
+
 # Lazy initialisation - created once per worker
 _engine = None
 _SessionLocal = None
@@ -32,10 +46,6 @@ def create_session():
             bind=get_engine(),
         )
     return _SessionLocal
-
-
-# Export callable for tests
-SessionLocal = create_session
 
 
 def get_db():
