28a6a9779eae04982219deebf39c60a1f179ae08
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-09-02 10:18:51 +0100

    fix: add more tests for subdivision relationships

--- geographies-api/tests/test_main.py
+++ geographies-api/tests/test_main.py
@@ -36,13 +36,12 @@ def test_read_geography_statistics(slug: str, expected_statistics: dict | None):
 @pytest.mark.parametrize(
     ("slug", "has_subconcept_of"),
     [
-        # There are no subconcepts for regions
+        # There are no `subconcept_of` for regions
         ("sub-saharan-africa", False),
         # we do no _currently_ support subconcepts on countries
         # TODO: add Region `subconcept_of` to Country
         ("north-america", False),
-        # we do no _currently_ support subconcepts on countries
-        # TODO: add Region `subconcept_of` to Country
+        # we do support `subconcept_of` on subdivisions
         ("us-ca", True),
     ],
 )
@@ -56,8 +55,38 @@ def test_read_geography_subconcept(slug: str, has_subconcept_of: bool):
     # we should never include the relationship in the list response
     assert "subconcept_of" not in geography
 
-    # check if we have the subconcept_of in the right place
+    # check if we have the subconcept_of on supported item pages
     item_response = client.get(f"/geographies/{slug}")
     item_json = item_response.json()
     geography = item_json["data"]
     assert ("subconcept_of" in geography) == has_subconcept_of
+
+
+@pytest.mark.parametrize(
+    ("slug", "has_related_concepts"),
+    [
+        # We do no show related_concepts for regions
+        ("sub-saharan-africa", False),
+        # we do no support `related_concepts` on countries as
+        # 1. the response would be very large
+        # 2. we do not have a use case for it to incur ☝️ cost
+        ("north-america", False),
+        # we do support `related_concepts` on subdivisions
+        ("us-ca", True),
+    ],
+)
+def test_read_geography_related_concepts(slug: str, has_related_concepts: bool):
+    list_response = client.get("/geographies/")
+    list_json = list_response.json()
+    geography = next(
+        geography for geography in list_json["data"] if geography["slug"] == slug
+    )
+    assert geography is not None
+    # we should never include the relationship in the list response
+    assert "related_concepts" not in geography
+
+    # check if we have the related_concepts in the right place
+    item_response = client.get(f"/geographies/{slug}")
+    item_json = item_response.json()
+    geography = item_json["data"]
+    assert ("related_concepts" in geography) == has_related_concepts
