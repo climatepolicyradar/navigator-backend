caec0196ccbdccf67173ada9ae7b91680c9094a1
Anna Pokorska <anna@climatepolicyradar.org>
2025-10-21 16:56:18 +0100

    Add test case for a global CCC family

--- backend-api/tests/non_search/service/download/test_download_search.py
+++ backend-api/tests/non_search/service/download/test_download_search.py
@@ -133,32 +133,52 @@ def test_process_result_into_csv_returns_correct_data_for_CCC_search_in_csv_form
 ):
     mock_organisation = "CCC"
 
-    mock_family_slug = "ccc-test-family"
+    mock_family_slug_us = "ccc-test-family-us"
 
-    mock_document_slug = "ccc-test-document"
-    mock_document_source_url = "www.ccc-test-document.pdf"
-    mock_document_title = "CCC Test Document"
+    mock_document_slug_us = "ccc-test-document-us"
+    mock_document_source_url_us = "www.ccc-test-document-us.pdf"
+    mock_document_title_us = "CCC Test Document US"
+    mock_document_import_id_us = "Test.CCC.document.0"
+
+    mock_family_slug_global = "ccc-test-family-global"
+
+    mock_document_slug_global = "ccc-test-document-global"
+    mock_document_source_url_global = "www.ccc-test-document-global.pdf"
+    mock_document_title_global = "CCC Test Document Global"
+    mock_document_import_id_global = "Test.CCC.document.1"
+
     mock_document_content_type = "application/pdf"
-    mock_document_import_id = "Test.CCC.document.0"
 
     mock_get_extra_csv_info.return_value = {
-        "source": {mock_family_slug: mock_organisation},
+        "source": {mock_family_slug_us: mock_organisation},
         "documents": {
-            mock_family_slug: [
+            mock_family_slug_us: [
                 FamilyDocument(
-                    import_id=mock_document_import_id,
-                    slugs=[Slug(name=mock_document_slug)],
+                    import_id=mock_document_import_id_us,
+                    slugs=[Slug(name=mock_document_slug_us)],
                     physical_document=PhysicalDocument(
-                        source_url=mock_document_source_url,
-                        title=mock_document_title,
+                        source_url=mock_document_source_url_us,
+                        title=mock_document_title_us,
                         content_type=mock_document_content_type,
                     ),
                     valid_metadata={},
                 )
-            ]
+            ],
+            mock_family_slug_global: [
+                FamilyDocument(
+                    import_id=mock_document_import_id_global,
+                    slugs=[Slug(name=mock_document_slug_global)],
+                    physical_document=PhysicalDocument(
+                        source_url=mock_document_source_url_global,
+                        title=mock_document_title_global,
+                        content_type=mock_document_content_type,
+                    ),
+                    valid_metadata={},
+                )
+            ],
         },
         "collection": {
-            mock_family_slug: Collection(
+            mock_family_slug_us: Collection(
                 import_id=f"Test.{mock_organisation}.collection.0",
                 title=f"Test {mock_organisation} Collection",
                 description=f"Test {mock_organisation} Description",
@@ -166,23 +186,32 @@ def test_process_result_into_csv_returns_correct_data_for_CCC_search_in_csv_form
             )
         },
         "document_events": {
-            mock_document_import_id: [
+            mock_document_import_id_us: [
                 FamilyEvent(
                     valid_metadata={
-                        "event_type": ["Test Type"],
-                        "description": ["Test Description"],
+                        "event_type": ["Test Type US"],
+                        "description": ["Test Description US"],
                     },
                     date=datetime(2025, 2, 2),
                 )
-            ]
+            ],
+            mock_document_import_id_global: [
+                FamilyEvent(
+                    valid_metadata={
+                        "event_type": ["Test Type Global"],
+                        "description": ["Test Description Global"],
+                    },
+                    date=datetime(2024, 2, 2),
+                )
+            ],
         },
     }
 
     families = [
         SearchResponseFamily(
-            family_slug=mock_family_slug,
-            family_name=f"{mock_organisation} Test Family",
-            family_description=f"{mock_organisation} Test Family Description",
+            family_slug=mock_family_slug_us,
+            family_name=f"{mock_organisation} Test Family US",
+            family_description=f"{mock_organisation} Test Family Description US",
             family_category="Litigation",
             family_date="2025-01-01",
             family_source=mock_organisation,  # ORG
@@ -193,9 +222,9 @@ def test_process_result_into_csv_returns_correct_data_for_CCC_search_in_csv_form
                 "case_number": ["00-1111"],
                 "status": ["Decided"],
                 "concept_preferred_label": [
-                    "category/Test Category",
-                    "principal_law/Test Principal Law",
-                    "jurisdiction/Test Jurisdiction",
+                    "category/Test Category US",
+                    "principal_law/Test Principal Law US",
+                    "jurisdiction/Test Jurisdiction US",
                 ],
             },
             family_title_match=True,
@@ -203,11 +232,11 @@ def test_process_result_into_csv_returns_correct_data_for_CCC_search_in_csv_form
             total_passage_hits=1,
             family_documents=[
                 SearchResponseFamilyDocument(
-                    document_title=mock_document_title,
-                    document_slug=mock_document_slug,
-                    document_type="Test",
-                    document_source_url=mock_document_source_url,
-                    document_url=f"https://test.com/documents/{mock_document_slug}",
+                    document_title=mock_document_title_us,
+                    document_slug=mock_document_slug_us,
+                    document_type="Test US",
+                    document_source_url=mock_document_source_url_us,
+                    document_url=f"https://test.com/documents/{mock_document_slug_us}",
                     document_content_type=mock_document_content_type,
                     document_passage_matches=[
                         SearchResponseDocumentPassage(text="test", text_block_id="0")
@@ -217,7 +246,48 @@ def test_process_result_into_csv_returns_correct_data_for_CCC_search_in_csv_form
             continuation_token=None,
             prev_continuation_token=None,
             metadata=None,
-        )
+        ),
+        SearchResponseFamily(
+            family_slug=mock_family_slug_global,
+            family_name=f"{mock_organisation} Test Family Global",
+            family_description=f"{mock_organisation} Test Family Description Global",
+            family_category="Litigation",
+            family_date="2024-01-01",
+            family_source=mock_organisation,  # ORG
+            corpus_import_id=f"Test.{mock_organisation}.corpus.0",
+            corpus_type_name="Litigation",
+            family_geographies=["BRA"],
+            family_metadata={
+                "case_number": ["00-2222"],
+                "status": ["Filed"],
+                "core_object": ["Test Core Object"],
+                "original_case_name": ["Test Non-English Case Name"],
+                "concept_preferred_label": [
+                    "category/Test Category Global",
+                    "principal_law/Test Principal Law Global",
+                    "jurisdiction/Test Jurisdiction Global",
+                ],
+            },
+            family_title_match=True,
+            family_description_match=False,
+            total_passage_hits=1,
+            family_documents=[
+                SearchResponseFamilyDocument(
+                    document_title=mock_document_title_global,
+                    document_slug=mock_document_slug_global,
+                    document_type="Test Global",
+                    document_source_url=mock_document_source_url_global,
+                    document_url=f"https://test.com/documents/{mock_document_slug_global}",
+                    document_content_type=mock_document_content_type,
+                    document_passage_matches=[
+                        SearchResponseDocumentPassage(text="test", text_block_id="0")
+                    ],
+                )
+            ],
+            continuation_token=None,
+            prev_continuation_token=None,
+            metadata=None,
+        ),
     ]
 
     expected_search_csv = (
@@ -250,28 +320,52 @@ def test_process_result_into_csv_returns_correct_data_for_CCC_search_in_csv_form
         "Test.CCC.collection.0,"  # 1
         "Test CCC Collection,"  # 2
         "https://test.com/collection/Test.CCC.collection.0,"  # 3
-        "ccc-test-family,"  # 4
-        "CCC Test Family,"  # 5
-        ","  # 6 GLOBAL field - no value for US families
-        "https://test.com/document/ccc-test-family,"  # 7
+        "ccc-test-family-us,"  # 4
+        "CCC Test Family US,"  # 5
+        ","  # 6 GLOBAL field only - no value for US families
+        "https://test.com/document/ccc-test-family-us,"  # 7
         "Test CCC Description,"  # 8
-        "CCC Test Family Description,"  # 9
+        "CCC Test Family Description US,"  # 9
         "00-1111,"  # 10
         "2025,"  # 11
-        "Decided,"  # 12 metadata
-        "Test Jurisdiction,"  # 13
-        "Test Category,"  # 14
-        "Test Principal Law,"  # 15
+        "Decided,"  # 12
+        "Test Jurisdiction US,"  # 13
+        "Test Category US,"  # 14
+        "Test Principal Law US,"  # 15
         ","  # 16 court number is not a thing
-        "CCC Test Document,"  # 17
-        "https://test.com/documents/ccc-test-document,"  # 18
-        "www.ccc-test-document.pdf,"  # 19
-        "Test Type,"  # 20
+        "CCC Test Document US,"  # 17
+        "https://test.com/documents/ccc-test-document-us,"  # 18
+        "www.ccc-test-document-us.pdf,"  # 19
+        "Test Type US,"  # 20
         "2025-02-02T00:00:00,"  # 21
-        "Test Description,"  # 22
+        "Test Description US,"  # 22
         "USA,"  # 23
         "Yes\r\n"  # 24
         # Row 2 - Corresponding values - GLOBAL family
+        ","  # 1
+        ","  # 2
+        ","  # 3
+        "ccc-test-family-global,"  # 4
+        "CCC Test Family Global,"  # 5
+        "Test Non-English Case Name,"  # 6
+        "https://test.com/document/ccc-test-family-global,"  # 7
+        "Test Core Object,"  # 8
+        "CCC Test Family Description Global,"  # 9
+        "00-2222,"  # 10
+        "2024,"  # 11
+        "Filed,"  # 12
+        "Test Jurisdiction Global,"  # 13
+        "Test Category Global,"  # 14
+        "Test Principal Law Global,"  # 15
+        ","  # 16 court number is not a thing
+        "CCC Test Document Global,"  # 17
+        "https://test.com/documents/ccc-test-document-global,"  # 18
+        "www.ccc-test-document-global.pdf,"  # 19
+        "Test Type Global,"  # 20
+        "2024-02-02T00:00:00,"  # 21
+        "Test Description Global,"  # 22
+        "BRA,"  # 23
+        "Yes\r\n"  # 24
     )
 
     assert (
