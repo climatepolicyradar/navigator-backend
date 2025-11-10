3f40023fb06359fa3d50a02289fa4f4adaea87ba
James Gorrie <jamesgorrie@users.noreply.github.com>
2025-11-10 14:11:55 +0000

    fix: very basic transformation pattern (#797)
    * fix: very basic transformation pattern
    
    * fix: update navigator_family_etl_pipeline
    
    * fix(data-in-pipeline): type test mocks
    
    * fix(data-in-pipeline): merge

--- data-in-pipeline/app/transform/navigator_document.py
+++ data-in-pipeline/app/transform/navigator_document.py
@@ -3,4 +3,4 @@ from app.models import Document, Identified
 
 
 def transform_navigator_document(input: Identified[NavigatorDocument]) -> Document:
-    return Document(id=input.id)
+    return Document(id=input.id, title=input.data.title)
--- data-in-pipeline/app/transform/navigator_family.py
+++ data-in-pipeline/app/transform/navigator_family.py
@@ -1,6 +1,67 @@
+from returns.result import Failure, Result, Success
+
 from app.extract.connectors import NavigatorFamily
 from app.models import Document, Identified
+from app.transform.models import CouldNotTransform, NoMatchingTransformations
 
 
-def transform_navigator_family(input: Identified[NavigatorFamily]) -> Document:
-    return Document(id=input.id)
+def transform_navigator_family(
+    input: Identified[NavigatorFamily],
+) -> Result[Document, NoMatchingTransformations]:
+    transformers = [
+        transform_navigator_family_with_single_matching_document,
+        transform_navigator_family_never,
+    ]
+
+    success = None
+    failures = []
+    for transformer in transformers:
+        result = transformer(input)
+        match result:
+            case Success(document):
+                success = document
+            case Failure(error):
+                failures.append(error)
+
+    if success:
+        return Success(success)
+    else:
+        return Failure(
+            NoMatchingTransformations(
+                f"No matching transformations found for {input.id}"
+            )
+        )
+
+
+def transform_navigator_family_with_single_matching_document(
+    input: Identified[NavigatorFamily],
+) -> Result[Document, CouldNotTransform]:
+    """
+    We have some document <=> family relationships that are essentially just a
+    repeat of each other.
+
+    We can use this 1-1 mapping and put the data from the family and store on the document.
+
+    At time of counting this was ~9000 documents.
+    """
+    if (
+        len(input.data.documents) == 1
+        and input.data.documents[0].title == input.data.title
+    ):
+        return Success(Document(id=input.id, title=input.data.title))
+    else:
+        return Failure(
+            CouldNotTransform(
+                f"transform_navigator_family_with_single_matching_document could not transform {input.id}"
+            )
+        )
+
+
+def transform_navigator_family_never(
+    input: Identified[NavigatorFamily],
+) -> Result[Document, CouldNotTransform]:
+    return Failure(
+        CouldNotTransform(
+            f"transform_navigator_family_never could not transform {input.id}"
+        )
+    )
