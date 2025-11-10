ddc2818dc9fc0cd495d3ff29bcbf084dca8e4a88
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-11-10 18:13:35 +0000

    fix(data-in-pipeline): better comments on scripts mnade for local dev flow

--- data-in-pipeline/app/scripts/cache_all_navigator_families.py
+++ data-in-pipeline/app/scripts/cache_all_navigator_families.py
@@ -6,7 +6,7 @@ import requests
 
 def cache_all_navigator_families():
     page = 1
-    ids = []
+    family_import_ids = []
     families = []
     while True:
         response = requests.get(
@@ -18,12 +18,12 @@ def cache_all_navigator_families():
         if len(data["data"]) == 0:
             break
         page += 1
-        ids.extend([item["import_id"] for item in data["data"]])
+        family_import_ids.extend([item["import_id"] for item in data["data"]])
         families.extend(data["data"])
 
     os.makedirs(".data_cache", exist_ok=True)
     with open(".data_cache/navigator_family_ids.json", "w") as f:
-        json.dump(ids, f, indent=4)
+        json.dump(family_import_ids, f, indent=4)
     with open(".data_cache/navigator_families.json", "w") as f:
         json.dump(families, f, indent=4)
-    return ids
+    return family_import_ids
