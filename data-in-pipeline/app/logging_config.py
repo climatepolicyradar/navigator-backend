8957c51918189a2ddfd6b084fd2fdcacd5ed5aa8
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-11-11 12:02:35 +0000

    Feature/app 1411 get otel prefect and python loggers to emit the same things (#801)
    * Default to debug logging
    
    * Update logging to make debugging easier
    
    * Fix just_dev
    
    * Add default Python logging
    
    * Update test_unit.py
    
    * Update test_unit.py
    
    * Change PREFECT_LOGGING_LEVEL from DEBUG to WARNING
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Add logging instrumentor
    
    * Update logging config
    
    * Revert "Add logging instrumentor"
    
    This reverts commit bbd5f9bd60179b4d5c10cef787c6e8b1531ef4e5.
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Add test dockerfile
    
    * Update conftest.py
    
    * Add otel logging instrumentor
    
    * Implement otel logging instrumentor
    
    * Implement otel logging instrumentor
    
    * Revert to see if logging changes worked without deploying
    
    * Revert "Revert to see if logging changes worked without deploying"
    
    This reverts commit 38ea5a69395a6e6b9516d0a07bbb26f05c3fe319.
    
    * Update Dockerfile
    
    * Update uv.lock
    
    * Fix dev
    
    * Disable otel logging for dev & test runs

--- data-in-pipeline/app/logging_config.py
+++ data-in-pipeline/app/logging_config.py
@@ -2,6 +2,8 @@ import logging
 import logging.config
 import os
 
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+
 LOG_LEVEL = os.getenv("OTEL_PYTHON_LOG_LEVEL", "INFO").upper()
 ENV = os.getenv("ENV", "production")
 
@@ -22,3 +24,13 @@ DEFAULT_LOGGING = {
 }
 _LOGGER = logging.getLogger(__name__)
 logging.config.dictConfig(DEFAULT_LOGGING)
+
+if os.getenv("DISABLE_OTEL_LOGGING", "true").lower() == "false":
+    try:
+        LoggingInstrumentor().instrument(set_logging_format=False)
+        _LOGGER.debug("Enabled OpenTelemetry logging instrumentor")
+    except Exception as exc:  # noqa: BLE001
+        _LOGGER.exception("Failed to enable OpenTelemetry logging instrumentor")
+        raise RuntimeError("OpenTelemetry logging instrumentor failed") from exc
+else:
+    _LOGGER.debug("OpenTelemetry logging instrumentor disabled")
