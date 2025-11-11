c30c30ac4200dacd6fc48adad3aeb033478eef83
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-11-11 17:02:59 +0000

    Update logging_config.py

--- data-in-pipeline/app/logging_config.py
+++ data-in-pipeline/app/logging_config.py
@@ -5,28 +5,9 @@ import os
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
 LOG_LEVEL = os.getenv("OTEL_PYTHON_LOG_LEVEL", "INFO").upper()
-ENV = os.getenv("ENV", "production")
-
-# LOG_LEVEL = os.getenv("OTEL_PYTHON_LOG_LEVEL", "INFO").upper()
 ENV = os.getenv("ENV", "development")
 
-# DEFAULT_LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "handlers": {
-#         "console": {
-#             "class": "logging.StreamHandler",
-#             "stream": "ext://sys.stdout",  # Default is stderr
-#         },
-#     },
-#     "loggers": {},
-#     "root": {
-#         "handlers": ["console"],
-#         "level": LOG_LEVEL,
-#     },
-# }
 _LOGGER = logging.getLogger(__name__)
-# logging.config.dictConfig(DEFAULT_LOGGING)
 is_logging_enabled = os.getenv("DISABLE_OTEL_LOGGING", "true").lower() == "false"
 if is_logging_enabled:
     try:
