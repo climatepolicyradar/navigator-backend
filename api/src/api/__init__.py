47774c2fb981ab6b8c6b19b766a6ad53ada1ae5b
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-11-13 13:46:22 +0000

    Update __init__.py

--- api/src/api/__init__.py
+++ api/src/api/__init__.py
@@ -1,10 +1,12 @@
 from api.telemetry_base import BaseTelemetry
 from api.telemetry_fastapi import FastAPITelemetry
+from api.telemetry_prefect import PrefectTelemetry
 from api.telemetry_utils import convert_to_loggable_string, observe
 
 __all__ = [
     "BaseTelemetry",
     "FastAPITelemetry",
+    "PrefectTelemetry",
     "convert_to_loggable_string",
     "observe",
 ]
