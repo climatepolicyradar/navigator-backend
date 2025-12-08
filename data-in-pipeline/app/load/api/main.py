fbf7ee508a960b2eb3bd837da851022c5ea01b6f
Anna Pokorska <anna@climatepolicyradar.org>
2025-12-08 16:09:45 +0000

    Simplify

--- data-in-pipeline/app/load/api/main.py
+++ data-in-pipeline/app/load/api/main.py
@@ -1,10 +1,8 @@
 import logging
 from pathlib import Path
 
-from api.router import router as documents_router
 from api.settings import settings
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import APIRouter, FastAPI
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,39 +17,19 @@ app = FastAPI(
     openapi_url="/documents/openapi.json",
 )
 
-# Include custom routers in our app
-app.include_router(documents_router)
+router = APIRouter(prefix="/documents")
+app.include_router(router)
 
 
-# Add CORS middleware to allow cross origin requests from any port
-_ALLOW_ORIGIN_REGEX = (
-    r"http://localhost:3000|"
-    r"http://bs-local.com:3000|"
-    r"https://.+\.climatepolicyradar\.org|"
-    r"https://.+\.staging.climatepolicyradar\.org|"
-    r"https://.+\.production.climatepolicyradar\.org|"
-    r"https://.+\.sandbox\.climatepolicyradar\.org|"
-    r"https://climate-laws\.org|"
-    r"https://.+\.climate-laws\.org|"
-    r"https://climateprojectexplorer\.org|"
-    r"https://.+\.climateprojectexplorer\.org|"
-    r"https://.+\.climatecasechart\.com"
-)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origin_regex=_ALLOW_ORIGIN_REGEX,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-# We use both routers to make sure we can have /families/health available publicly
-# and /health available to the internal network & AppRunner health check.
 @app.get("/health")
-@documents_router.get("/health")
+@router.get("/health")
 def health_check():
     return {
         "status": "ok",
         "version": settings.github_sha,  # @related: GITHUB_SHA_ENV_VAR
     }
+
+
+@router.post("/")
+def create_document():
+    return ""
