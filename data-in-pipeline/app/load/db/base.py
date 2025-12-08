d79c47e2d6b44a1bb4ba3f6bde30f8549e47d82b
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-12-08 10:15:37 +0000

    Update base.py

--- data-in-pipeline/app/load/db/base.py
+++ data-in-pipeline/app/load/db/base.py
@@ -1,8 +1,16 @@
 import logging
+import os
 
 from sqlalchemy.orm import registry
 
 _LOGGER = logging.getLogger(__name__)
+
+
+def get_library_path() -> str:
+    """Return the absolute path of the library already installed"""
+    script_path = os.path.realpath(__file__)
+    script_directory = os.path.dirname(script_path)
+    return script_directory
 
 
 def make_declarative_base():
