db2ba5cc04438c13843bb93acaab19658f58c53d
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-01-05 14:17:11 +0000

    Revert "Update __init__.py"
    This reverts commit 238a6c0458bc0dd889f814c9f36cf903fb7e0495.

--- api/src/api/__init__.py
+++ api/src/api/__init__.py
@@ -2,7 +2,6 @@ from api.base_telemetry import BaseTelemetry
 from api.fastapi_telemetry import FastAPITelemetry
 from api.metrics import MetricsService
 from api.service_manifest import ServiceManifest
-from api.sqlalchemy_telemetry import SQLAlchemyTelemetry
 from api.telemetry_config import TelemetryConfig
 from api.telemetry_utils import convert_to_loggable_string, observe
 
@@ -11,7 +10,6 @@ __all__ = [
     "FastAPITelemetry",
     "MetricsService",
     "ServiceManifest",
-    "SQLAlchemyTelemetry",
     "TelemetryConfig",
     "convert_to_loggable_string",
     "observe",
