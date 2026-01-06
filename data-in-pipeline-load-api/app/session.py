0e8d3d92e9596289b6953a2c17e5e623086612d2
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-01-06 17:23:39 +0000

    Remove commit() from context manager (#898)
    * Remove commit from context manager
    
    * Update session.py

--- data-in-pipeline-load-api/app/session.py
+++ data-in-pipeline-load-api/app/session.py
@@ -63,11 +63,10 @@ def get_db_context() -> Generator[Session, None, None]:
     try:
         _LOGGER.debug("Database session created (context manager)")
         yield db
-        db.commit()
-        _LOGGER.debug("Database session committed")
+        _LOGGER.debug("Safely exiting database session (context manager)")
     except Exception:
         db.rollback()
-        _LOGGER.exception("Database session rolled back")
+        _LOGGER.exception("Database session rolled back (context manager)")
         raise
     finally:
         db.close()
