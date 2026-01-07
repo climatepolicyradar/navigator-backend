ad1f4910369d182a76dd31db434eef93e230cc28
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-01-07 10:39:34 +0000

    Delete files

--- data-in-pipeline/app/load/api/settings.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    github_sha: str = "unknown"
-
-
-# pydantic settings are set from the env variables passed in via docker / apprunner
-settings = Settings()
