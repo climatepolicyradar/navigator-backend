eddb88c62cb1d7b1b1b618eb96992cf1dbc96eaa
James Gorrie <james.gorrie@climatepolicyradar.org>
2026-01-13 12:53:32 +0000

    fix(dip): use WithDbDatetimeFields

--- data-in-pipeline/app/db_models.py
+++ data-in-pipeline/app/db_models.py
@@ -1,69 +1,56 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
-from sqlalchemy import Column, DateTime, func
 from sqlmodel import Field, Relationship, SQLModel
 
 
-class Document(SQLModel, table=True):
+class WithDbDatetimeFields(SQLModel):
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(UTC),
+    )
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(UTC),
+        sa_column_kwargs={"onupdate": lambda: datetime.now(UTC)},
+    )
+
+
+class Document(WithDbDatetimeFields, table=True):
     id: str = Field(primary_key=True)
     title: str
     description: str | None = None
-    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
-    updated_at: datetime = Field(
-        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
-    )
-    items: list["Item"] = Relationship(back_populates="document")
 
+    items: list["Item"] = Relationship(back_populates="document")
     labels: list["DocumentLabelLink"] = Relationship(back_populates="document")
 
 
-class Label(SQLModel, table=True):
+class Label(WithDbDatetimeFields, table=True):
     id: str = Field(primary_key=True)
     title: str
     type: str
 
     documents: list["DocumentLabelLink"] = Relationship(back_populates="label")
 
-    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
-    updated_at: datetime = Field(
-        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
-    )
 
-
-class DocumentLabelLink(SQLModel, table=True):
-    document_id: str = Field(foreign_key="document.id", primary_key=True)
-    label_id: str = Field(foreign_key="label.id", primary_key=True)
+class DocumentLabelLink(WithDbDatetimeFields, table=True):
     relationship_type: str
     timestamp: datetime | None = None
+    document_id: str = Field(foreign_key="document.id", primary_key=True)
+    label_id: str = Field(foreign_key="label.id", primary_key=True)
 
     label: Label = Relationship(back_populates="documents")
     document: Document = Relationship(back_populates="labels")
 
-    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
-    updated_at: datetime = Field(
-        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
-    )
 
-
-class DocumentDocumentLink(SQLModel, table=True):
-    source_document_id: str = Field(foreign_key="document.id", primary_key=True)
-    related_document_id: str = Field(foreign_key="document.id", primary_key=True)
+class DocumentDocumentLink(WithDbDatetimeFields, table=True):
     relationship_type: str
     timestamp: datetime | None = None
-    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
-    updated_at: datetime = Field(
-        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
-    )
+
+    source_document_id: str = Field(foreign_key="document.id", primary_key=True)
+    related_document_id: str = Field(foreign_key="document.id", primary_key=True)
 
 
-class Item(SQLModel, table=True):
+class Item(WithDbDatetimeFields, table=True):
+    url: str | None = None
     id: str = Field(primary_key=True)
     document_id: str = Field(foreign_key="document.id")
-    url: str | None = None
 
     document: Document = Relationship(back_populates="items")
-
-    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
-    updated_at: datetime = Field(
-        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
-    )
