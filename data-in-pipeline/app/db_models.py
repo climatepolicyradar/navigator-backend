23a141f913686e9d0bd3aa63a0a6e35afde86f3c
Osneil Drakes <osneildrakes@MacBookPro.communityfibre.co.uk>
2026-01-13 11:44:43 +0000

    refactor: switch up how we are handling the created and updated at fields

--- data-in-pipeline/app/db_models.py
+++ data-in-pipeline/app/db_models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from sqlalchemy import Column, DateTime, func
 from sqlmodel import Field, Relationship, SQLModel
 
 
@@ -7,8 +8,10 @@ class Document(SQLModel, table=True):
     id: str = Field(primary_key=True)
     title: str
     description: str | None = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
+    updated_at: datetime = Field(
+        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
+    )
     items: list["Item"] = Relationship(back_populates="document")
 
     labels: list["DocumentLabelLink"] = Relationship(back_populates="document")
@@ -21,8 +24,10 @@ class Label(SQLModel, table=True):
 
     documents: list["DocumentLabelLink"] = Relationship(back_populates="label")
 
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
+    updated_at: datetime = Field(
+        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
+    )
 
 
 class DocumentLabelLink(SQLModel, table=True):
@@ -34,8 +39,10 @@ class DocumentLabelLink(SQLModel, table=True):
     label: Label = Relationship(back_populates="documents")
     document: Document = Relationship(back_populates="labels")
 
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
+    updated_at: datetime = Field(
+        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
+    )
 
 
 class DocumentDocumentLink(SQLModel, table=True):
@@ -43,15 +50,20 @@ class DocumentDocumentLink(SQLModel, table=True):
     related_document_id: str = Field(foreign_key="document.id", primary_key=True)
     relationship_type: str
     timestamp: datetime | None = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
+    updated_at: datetime = Field(
+        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
+    )
 
 
 class Item(SQLModel, table=True):
     id: str = Field(primary_key=True)
     document_id: str = Field(foreign_key="document.id")
     url: str | None = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
 
     document: Document = Relationship(back_populates="items")
+
+    created_at: datetime = Field(sa_column=Column(DateTime, default=func.now()))
+    updated_at: datetime = Field(
+        sa_column=Column(DateTime, default=func.now(), onupdate=func.now())
+    )
