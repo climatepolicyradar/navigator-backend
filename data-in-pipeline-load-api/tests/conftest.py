bf4431ebba8d4a604d2f049a2e54eb7718b76c82
James Gorrie <james.gorrie@climatepolicyradar.org>
2026-01-15 11:11:37 +0000

    fix(dip): remove run_migrations test from load-api

--- data-in-pipeline-load-api/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pytest_mock_resources import PostgresConfig
-
-
-# Configuration of pytest mock postgres fixtures
-@pytest.fixture(scope="session")
-def pmr_postgres_config():
-    return PostgresConfig(image="postgres:17")
