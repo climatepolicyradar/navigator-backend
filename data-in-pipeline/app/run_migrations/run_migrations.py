9d7bf833e76ef388c1cab4a99c1342f558d56c2a
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-01-20 18:12:41 +0000

    Update run_migrations.py

--- data-in-pipeline/app/run_migrations/run_migrations.py
+++ data-in-pipeline/app/run_migrations/run_migrations.py
@@ -1,16 +1,38 @@
+import os
+
+import requests
+
 from app.bootstrap_telemetry import get_logger
 
 
 def run_migrations() -> None:
-    """
-    Apply alembic migrations.
+    """Apply alembic migrations.
 
-    Call through subprocess as opposed to the alembic command function as the server
-    startup never completed when using the alembic solution.
+    Call through subprocess as opposed to the alembic command function
+    as the server startup never completed when using the alembic
+    solution.
 
-    TODO: https://linear.app/climate-policy-radar/issue/APP-1600/get-a-working-db-url-into-run-migrations
-    Completing this method is dependant on the above.
+    TODO: APP-1578 -Completing this method according to the agreed upon
+    design is dependant on 1578 being unblocked. For now this is a
+    temporary solution that uses the load API to run migrations, which
+    unblocks development work.
     """
 
     logger = get_logger()
-    logger.info("run_migrations")
+
+    try:
+        # Ensure URL has a scheme - App Runner URLs may not include it
+        load_api_base_url = os.getenv("DATA_IN_PIPELINE_LOAD_API_URL", "")
+        if not load_api_base_url.startswith(("http://", "https://")):
+            load_api_base_url = f"https://{load_api_base_url}"
+
+        logger.info("Running migrations via load API...")
+        response = requests.post(
+            url=f"{load_api_base_url}/load/run-migrations",
+            timeout=10,  # seconds
+        )
+        response.raise_for_status()
+    except Exception as e:
+        logger.exception("Error running migrations via load API")
+        raise e
+    logger.info("Finished running Load DB schema migrations")
