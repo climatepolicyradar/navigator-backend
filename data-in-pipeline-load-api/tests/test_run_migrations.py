c8b3485f6eebca8538a4d9610625802e7ec23e2e
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-01-20 14:56:50 +0000

    Fix test runs to split out alembic tests

--- data-in-pipeline-load-api/tests/test_run_migrations.py
+++ data-in-pipeline-load-api/tests/test_run_migrations.py
@@ -1,27 +1,32 @@
+import pytest
 from pytest_alembic import tests as test_alembic
 
 
+@pytest.mark.alembic
 def test_model_definitions_match_ddl(alembic_runner):
     test_alembic.test_model_definitions_match_ddl(alembic_runner)
 
 
-# def test_single_head_revision(alembic_runner):
-#     test_alembic.test_single_head_revision(alembic_runner)
+@pytest.mark.alembic
+@pytest.mark.xfail(
+    reason="This test is failing because the first revision has not been created"
+)
+def test_single_head_revision(alembic_runner):
+    test_alembic.test_single_head_revision(alembic_runner)
 
 
-# def test_upgrade(alembic_runner):
-#     test_alembic.test_upgrade(alembic_runner)
+@pytest.mark.alembic
+def test_upgrade(alembic_runner):
+    test_alembic.test_upgrade(alembic_runner)
 
 
-# def test_up_down_consistency(alembic_runner):
-#     test_alembic.test_up_down_consistency(alembic_runner)
+@pytest.mark.alembic
+def test_up_down_consistency(alembic_runner):
+    test_alembic.test_up_down_consistency(alembic_runner)
 
 
-# # Experimental pytest-alembic tests
-# # https://pytest-alembic.readthedocs.io/en/latest/experimental_tests.html
-# def test_all_models_register_on_metadata(alembic_runner):
-#     test_alembic.all_models_register_on_metadata(alembic_runner)
-
-
-# def test_downgrade_leaves_no_trace(alembic_runner):
-#     test_alembic.test_downgrade_leaves_no_trace(alembic_runner)
+# Experimental pytest-alembic tests
+# https://pytest-alembic.readthedocs.io/en/latest/experimental_tests.html
+@pytest.mark.alembic
+def test_downgrade_leaves_no_trace(alembic_runner):
+    test_alembic.experimental.test_downgrade_leaves_no_trace(alembic_runner)  # type: ignore
