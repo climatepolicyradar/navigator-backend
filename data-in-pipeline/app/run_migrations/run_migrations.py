3c696b441ba85ee18d7ec235da19666005354435
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-01-20 18:21:10 +0000

    Update run_migrations.py

--- data-in-pipeline/app/run_migrations/run_migrations.py
+++ data-in-pipeline/app/run_migrations/run_migrations.py
@@ -20,19 +20,45 @@ def run_migrations() -> None:
 
     logger = get_logger()
 
-    try:
-        # Ensure URL has a scheme - App Runner URLs may not include it
-        load_api_base_url = os.getenv("DATA_IN_PIPELINE_LOAD_API_URL", "")
-        if not load_api_base_url.startswith(("http://", "https://")):
-            load_api_base_url = f"https://{load_api_base_url}"
+    # Ensure URL has a scheme - App Runner URLs may not include it
+    load_api_base_url = os.getenv("DATA_IN_PIPELINE_LOAD_API_URL", "")
+    if not load_api_base_url.startswith(("http://", "https://")):
+        load_api_base_url = f"https://{load_api_base_url}"
 
-        logger.info("Running migrations via load API...")
+    # Get the current and head versions of alembic from the load API
+    try:
+        response = requests.get(
+            url=f"{load_api_base_url}/load/schema-info",
+            timeout=2,  # seconds
+        )
+        response.raise_for_status()
+
+        schema_info = response.json()
+        current_version = schema_info["current_version"]
+        head_version = schema_info["head_version"]
+    except Exception as e:
+        logger.exception(f"Error getting schema info from load API: {e}")
+        raise e
+
+    if current_version == head_version:
+        logger.debug("No migrations to run, alembic is up to date")
+        return
+
+    # Run the migrations if not idempotent
+    try:
+        logger.debug(
+            f"Upgrading schema from {current_version} to {head_version} via load API..."
+        )
         response = requests.post(
             url=f"{load_api_base_url}/load/run-migrations",
             timeout=10,  # seconds
         )
         response.raise_for_status()
     except Exception as e:
-        logger.exception("Error running migrations via load API")
+        logger.exception(
+            f"Error upgrading schema from {current_version} to {head_version}  via load API"
+        )
         raise e
-    logger.info("Finished running Load DB schema migrations")
+    logger.info(
+        f"Finished upgrading schema from {current_version} to {head_version} via load API"
+    )
