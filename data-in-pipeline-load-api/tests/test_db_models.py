1726a6c109565dbaa11ab14a3e7e833a5e62b975
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-01-20 14:30:56 +0000

    Uncomment model tests

--- data-in-pipeline-load-api/tests/test_db_models.py
+++ data-in-pipeline-load-api/tests/test_db_models.py
@@ -1,28 +1,27 @@
-# from sqlmodel import Session, SQLModel
-
-# from app.db_models import Document, Label
-
-
-# def test_can_create_tables(engine):
-#     table_names = SQLModel.metadata.tables.keys()
-#     assert "document" in table_names
-#     assert "label" in table_names
-#     assert "documentlabellink" in table_names
-#     assert "documentdocumentlink" in table_names
-#     assert "item" in table_names
+from data_in_models.src.data_in_models.models import Document, Label
+from sqlmodel import Session, SQLModel
 
 
-# def test_can_insert_and_query(engine):
-#     with Session(engine) as session:
-#         document = Document(id="test-doc-1", title="Document 1")
-#         session.add(document)
-#         session.commit()
-#         session.refresh(document)
+def test_can_create_tables(engine):
+    table_names = SQLModel.metadata.tables.keys()
+    assert "document" in table_names
+    assert "label" in table_names
+    assert "documentlabellink" in table_names
+    assert "documentdocumentlink" in table_names
+    assert "item" in table_names
 
-#         label = Label(id="label_1", title="Label 1", type="Type A")
-#         session.add(label)
-#         session.commit()
 
-#         queried_user = session.get(Document, document.id)
-#         assert queried_user is not None
-#         assert queried_user.title == "Document 1"
+def test_can_insert_and_query(engine):
+    with Session(engine) as session:
+        document = Document(id="test-doc-1", title="Document 1")
+        session.add(document)
+        session.commit()
+        session.refresh(document)
+
+        label = Label(id="label_1", title="Label 1", type="Type A")
+        session.add(label)
+        session.commit()
+
+        queried_user = session.get(Document, document.id)
+        assert queried_user is not None
+        assert queried_user.title == "Document 1"
