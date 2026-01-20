6424a814feb25e00be74e01fe85baed3ca02df28
annaCPR <anna@climatepolicyradar.org>
2026-01-20 17:44:52 +0000

    Disable tls (#955)
    * Disable TLS for testing
    
    * Disable bandit rule
    
    * Comment out connectivity tests and fix load to s3
    
    * Switch to http
    
    * Fix post request
    
    * Fix double serialisation error
    
    * Remove trailing slash from the post route in the load API

--- data-in-pipeline-load-api/app/routers.py
+++ data-in-pipeline-load-api/app/routers.py
@@ -29,7 +29,7 @@ def health_check(db=Depends(get_db)):
     return {"status": "ok", "version": settings.github_sha}
 
 
-@router.post("/", response_model=list[str], status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=list[str], status_code=status.HTTP_201_CREATED)
 def create_document(documents: list[Document]):
     return [doc.id for doc in documents]
 
