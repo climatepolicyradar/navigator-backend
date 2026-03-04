13b4ecd7ce075bb78407abb05de5553ac8cade5f
James Gorrie <jamesgorrie@users.noreply.github.com>
2026-03-04 15:29:15 +0000

    Revert "fix: add document attributes" (#1120)
    Revert "fix: add document attributes (#1109)"
    
    This reverts commit 8960f6bc7d5cca2ac2ded79d6e95795ef6b896d2.

--- data-in-models/src/data_in_models/models.py
+++ data-in-models/src/data_in_models/models.py
@@ -1,19 +1,11 @@
-from __future__ import annotations
-
 from datetime import datetime
 
 from pydantic import BaseModel
 
 
 class WithRelationships(BaseModel):
-    labels: list[LabelRelationship] = []
-    documents: list[DocumentRelationship] = []
-
-
-class WithAttributes(BaseModel):
-    # the `key` of the `dict` will probably be managed via our knowledge managers
-    # and not be a free for all
-    attributes: dict[str, str | float | bool] = {}
+    labels: list["LabelRelationship"] = []
+    documents: list["DocumentRelationship"] = []
 
 
 class Label(WithRelationships):
@@ -32,7 +24,7 @@ class Item(BaseModel):
     url: str | None = None
 
 
-class BaseDocument(WithAttributes):
+class BaseDocument(BaseModel):
     id: str
     title: str
     description: str | None = None
@@ -43,7 +35,7 @@ class BaseDocument(WithAttributes):
 
 class DocumentRelationship(BaseModel):
     type: str
-    value: DocumentWithoutRelationships
+    value: "DocumentWithoutRelationships"
     timestamp: datetime | None = None
 
 
@@ -52,4 +44,4 @@ class Document(BaseDocument, WithRelationships):
 
 
 class DocumentWithoutRelationships(BaseDocument):
-    labels: list[LabelRelationship] = []
+    labels: list["LabelRelationship"] = []
