f29af9b926defc034f46c076f7cffa96f40fc51c
Osneil Drakes <osneildrakes@MacBookPro.communityfibre.co.uk>
2026-04-07 17:16:05 +0100

    chore: trunk crashes out over this. this is a temp fix

--- data-in-pipeline-load-api/app/repository.py
+++ data-in-pipeline-load-api/app/repository.py
@@ -340,7 +340,7 @@ def sync_label_relationships(
     # Delete old relationships
     delete_stmt = delete(DBLabelLabelLink).where(
         DBLabelLabelLink.label_id.in_(sub_concept_ids),
-        ~tuple_(DBLabelLabelLink.label_id, DBLabelLabelLink.related_label_id).in_(
+        ~tuple_(DBLabelLabelLink.label_id, DBLabelLabelLink.related_label_id).in_(  # type: ignore[attr-defined]
             label_relationship_ids
         ),
     )
