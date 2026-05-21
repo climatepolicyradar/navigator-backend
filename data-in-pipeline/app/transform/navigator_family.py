5255471ceae275539fef937aa42d98c1d4eb8297
odrakes-cpr <osneil@climatepolicyradar.org>
2026-05-21 11:50:42 +0100

    Fix create dedicated transformation warning for missing region mapping (#1245)
    * chore: update the factory
    
    * fix: skip sending warnings when looking up world bank regions with custom country ids
    
    * feat: update variable name
    
    * feat: create dedicate error warnings for missing regions
    
    ---------
    
    Co-authored-by: Osneil Drakes <osneildrakes@unknown.home>
    Co-authored-by: Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>

--- data-in-pipeline/app/transform/navigator_family.py
+++ data-in-pipeline/app/transform/navigator_family.py
@@ -31,6 +31,7 @@ from app.geographies import (
 from app.models import Identified, NavigatorConcept
 from app.transform.models import (
     CouldNotTransform,
+    MissingRegionMapping,
     TransformWarning,
     UnknownGeography,
     UnknownParentLabel,
@@ -629,7 +630,7 @@ def _transform_geographies(
                 )
             elif geography.id not in _eu_and_international_region_ids:
                 warnings.append(
-                    UnknownGeography(
+                    MissingRegionMapping(
                         family_import_id=navigator_family.import_id,
                         geography_id=geography.id,
                     )
