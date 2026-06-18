7fa77019a3dbb9ab159959aa352e7d36a6636cb6
odrakes-cpr <osneil@climatepolicyradar.org>
2026-06-18 14:30:46 +0100

    chore: Increase min task count to 2 across ECS express services (#1288)

--- concepts-api/infra/__main__.py
+++ concepts-api/infra/__main__.py
@@ -225,7 +225,7 @@ ecs_express_service = ExpressGatewayService(
         ExpressGatewayServiceScalingTargetArgs(
             auto_scaling_metric="AVERAGE_CPU",
             auto_scaling_target_value=70,
-            min_task_count=1,
+            min_task_count=2,
             max_task_count=4,
         ),
     ],
