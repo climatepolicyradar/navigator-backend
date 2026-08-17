2be009c04405368e50e86e88f6a9cda48b03a661
Mark Cottam <mark.cottam096@gmail.com>
2026-08-17 16:04:55 +0100

    feat: Removing what looks like an unecessary module.

--- data-in-pipeline/app/dev.py
+++
@@ -1,5 +0,0 @@
-import app.bootstrap_telemetry  # noqa: F401 - Initialize telemetry first
-from app.navigator_family_etl_pipeline import data_in_pipeline
-
-if __name__ == "__main__":
-    data_in_pipeline(ids=["CCLW.legislative.10695.6311"])
